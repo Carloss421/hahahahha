a1ddb927bd8bbc5a85a26d96c6d9b816d6340ae7
Glitch (hello-webpage) <none>
2020-04-27 10:16:40 +0000

    🌉💠 Checkpoint ./README.md:14097343/2877 ./events/disconnect.js:14097343/707 ./events/guildMemberAdd.js:14097343/182 ./events/guildMemberRemove.js:14097343/154 ./events/guildMemberUpdate.js:14097343/126 ./events/message.js:14097343/887 ./events/ready.js:14097343/526 ./events/reconnecting.js:14097343/90 ./komutlar/erkek.js:14097343/2320 ./komutlar/kız.js:14097343/2325 ./komutlar/nick.js:14097343/1244 ./util/eventLoader.js:14097343/190 ./util/util.js:14097343/3662 ./utils/errors.js:14097343/1477 ./ayarlar.json:14097343/70 ./bot.js:14097343/12379 ./davetkanallar.json:14097343/2 ./json.sqlite:14097343/199 ./package.json:14097343/3221

---
+++ komutlar/kız.js
@@ -0,0 +1,43 @@
+
+const Discord = require('discord.js');
+const db = require("quick.db")
+exports.run = async (client, message, args) => {
+ if (!message.member.roles.has('697349767096696844') && !message.member.hasPermission('ADMINISTRATOR')) return message.channel.sendEmbed(new Discord.RichEmbed().setDescription('Bu komutu kullanabilmek için gerekli yetkiye sahip değilsin!').setColor("Black"));
+  let kullanıcı = message.mentions.users.first()
+  if (!kullanıcı) return message.channel.sendEmbed(new Discord.RichEmbed().setDescription('Bir üye etiketlemelisin!').setColor("Black"));
+  let user = message.mentions.users.first();
+  let rol = message.mentions.roles.first()
+  let member = message.guild.member(kullanıcı)
+   let isim = args[1];
+      if(!isim) return message.channel.send("Lütfen bir isim girin!").then(m => m.delete(5000));
+   let yas = args[2];
+      if(!yas) return message.channel.send("Lütfen bir yaş girin!")
+await member.setNickname(`て ${isim} | ${yas}`);
+  member.addRole("697347587950379038"); //kadın1
+  member.removeRole("697349728114966528"); //kayıtsız
+  message.react('699593882983464992')
+     const kanal = message.guild.channels.find(c => c.id == "701810769204871199") 
+    const embed1 = new Discord.RichEmbed() 
+    .addField(`${member.user} **Hoşgeldin , Seninle Beraber** \`${member.guild.memberCount}\` **Üyeye Ulaştık.**`)
+    .setColor("BLACK")
+    .setFooter(message.author.tag ,message.author.avatarURL)
+    .setTimestamp()
+  let embed = new Discord.RichEmbed() 
+  .setColor("BLACK")
+  .addField(`${member.user} **adlı üyeye** <@&697347587950379038> **rolünü verip ismini**  \` ${isim} | ${yas}\` **olarak ayarladım!**`)                                                                             
+  .setFooter(message.author.tag ,message.author.avatarURL)
+  .setTimestamp()
+  return message.channel.send(embed).then(kanal.send(embed1))
+}
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["k" , "kadın"],
+  kategori: "Yetkili Komutları",
+  permLevel: 0
+}
+exports.help = {
+  name: 'kadın',
+  description: "Sunucuya kaydolmaya ne dersin ?",
+  usage: 'kayıt isim yaş'
+} 
