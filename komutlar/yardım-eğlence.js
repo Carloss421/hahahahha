da42a2ce8bfe48160331886f57134ad999bd8cfa
Glitch (hello-webpage) <none>
2021-04-18 12:54:23 +0000

    😀🚪 Checkpoint ./komutlar/yardım-kullanıcı.js:15334932/66 ./komutlar/yardım-eğlence.js:15334932/84 ./komutlar/kullanıbıINFO.js:15334932/4051

--- komutlar/yardım-eğlence.js
+++ komutlar/yardım-eğlence.js
@@ -6,12 +6,15 @@ let eğlence = new Discord.MessageEmbed()
 .setTitle('Alvi - Eğlence')
 .setColor('RANDOM')
 .setDescription(`
-:skull_crossbones: \`${ayarlar.prefix}wasted\` Öldün resmi gönderir.
-:cat: \`${ayarlar.prefix}kedi-ol\` Kedi olursunuz.
-:heart: \`${ayarlar.prefix}aşk-ölçer\` Aşk ölçer.
-:monkey: \`${ayarlar.prefix}maymun-ol\` Maymun olursunuz
-👑 \`${ayarlar.prefix}kral-ol\` Kral olursunuz.
-:desktop: \`${ayarlar.prefix}hackle\` Hacker olursunuz`)
+:skull_crossbones: ~~ \`${ayarlar.prefix}wasted\` Öldün resmi gönderir.~~
+:cat: ~~ \`${ayarlar.prefix}kedi-ol\` Kedi olursunuz.~~
+:heart: ~~ \`${ayarlar.prefix}aşk-ölçer\` Aşk ölçer.~~
+:monkey: ~~ \`${ayarlar.prefix}maymun-ol\` Maymun olursunuz~~
+👑~~ \`${ayarlar.prefix}kral-ol\` Kral olursunuz.~~
+:desktop: ~~\`${ayarlar.prefix}hackle\` Hacker olursunuz~~
+
+**YAKINDA KOMUTLAR AKTIF OLUCAKTIR!**
+`)
 message.channel.send(eğlence)
 };
 
--- komutlar/yardım-kullanıcı.js
+++ komutlar/yardım-kullanıcı.js
@@ -12,7 +12,7 @@ let embed = new Discord.MessageEmbed()
 🧰 \`${ayarlar.prefix}canlı-destek\` Destek alırsınız. Troll amaçlı kullanılırsa karalisteye alınırsınız.
 ⚠️ \`${ayarlar.prefix}hata-bildir\` Hatayı, açığı bildirebilirsiniz.
 📝 \`${ayarlar.prefix}öneri\` Bot için öneri'de bulunursunuz.
-:information_source: \`\`
+:information_source: \`${ayarlar.prefix}isstatistik\` Botun bilgilerini gösterir.
 `)
 message.channel.send(embed)
 };
