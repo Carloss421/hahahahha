1a57c1df0061f0037641f1f983201777d21895d4
Glitch (hello-webpage) <none>
2021-04-19 18:26:41 +0000

    ⏱📮 Checkpoint ./komutlar/istatistik.js:15334932/418

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -4,24 +4,22 @@ const moment = require("moment");
 require("moment-duration-format");
 
 exports.run = function(client, message, args, bot, size) {
-    const duration = moment
-    .duration(bot.uptime)
-    .format("D [gün], H [saat], m [dakika], s [saniye]");
+const duration = moment
+    .duration(client.uptime)
+    .format(" D [gün], H [saat], m [dakika], s [saniye]");
 let embed = new Discord.MessageEmbed()
 .setTitle("Alvi - İstatistik")
 .setColor("RANDOM")
-.setDescription(`
-
-**Bot Prefix** \`a!\`
+.addField(`**Bot Prefix** \`a!\`
 **Bot İsmi** \`Alvi\`
 **Bot Dili** :flag_tr:
 **Sahip Adı** \`${ayarlar.ownerNAME}\`
 **Sahip ID** \`${ayarlar.ownerID}\`
 **Sahip Discord Nick** \`${ayarlar.ownerDcname}\`
 **Kodlandığı Uygulama** \`Visual Studio Code (VSC)\`
-**Kodlandığı Dil** \`Node.JS - Discord.JS - JavaScript\`
-**Bot Uptime** \`${duration}\` 
-**Bot Ping** \`${client.ping}ms\`
+**Kodlandığı Dil** \`Node.JS - Discord.JS - JavaScript\``)
+.addField(`**Bot Uptime** \``, duration, `\``)
+.addField(`**Bot Ping** \``, client.pingms`)
 **Toplam Üye Sayısı** \`Ölçülemedi\`
 **Toplam Sunucu Sayısı** \`${bot.guilds.size.toLocaleString()}\`
 
