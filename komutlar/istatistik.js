931be478d93e6a9e5a3a8966e9d47352e0e20d86
Glitch (hello-webpage) <none>
2021-04-19 18:38:35 +0000

    🏭💑 Checkpoint ./komutlar/istatistik.js:15334932/680 ./komutlar/afk.js:15334932/11

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -3,28 +3,27 @@ const ayarlar = require('../ayarlar.json')
 const moment = require("moment");
 require("moment-duration-format");
 
-exports.run = function(client, message, args, bot, size) {
+exports.run = function(client, message, args) {
 const duration = moment
     .duration(client.uptime)
     .format(" D [gün], H [saat], m [dakika], s [saniye]");
 let embed = new Discord.MessageEmbed()
 .setTitle("Alvi - İstatistik")
 .setColor("RANDOM")
-.addField(`**Bot Prefix** \`a!\`
+.setDescription(`
+**Bot Prefix** \`a!\`
 **Bot İsmi** \`Alvi\`
 **Bot Dili** :flag_tr:
 **Sahip Adı** \`${ayarlar.ownerNAME}\`
 **Sahip ID** \`${ayarlar.ownerID}\`
 **Sahip Discord Nick** \`${ayarlar.ownerDcname}\`
 **Kodlandığı Uygulama** \`Visual Studio Code (VSC)\`
-**Kodlandığı Dil** \`Node.JS - Discord.JS - JavaScript\``)
-.addField(`**Bot Uptime** \``, duration, `\``)
-.addField(`**Bot Ping** \``, client.pingms`)
-**Toplam Üye Sayısı** \`Ölçülemedi\`
-**Toplam Sunucu Sayısı** \`${bot.guilds.size.toLocaleString()}\`
-
-
-
+**Kodlandığı Dil** \`Node.JS - Discord.JS - JavaScript\`
+**Bot Uptime** \`${duration}\`
+**Bot Ping** \`${client.ws.ping}ms\`
+**Toplam Üye Sayısı** \`${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}\`
+**Toplam Sunucu Sayısı** \`${client.guilds.cache.size}\`
+**Toplam Kanal Sayısı** \`${client.channels.cache.size}\`
 **Verileri alınan komutlar**
 \`Otorol - Sayaç - KayıtSistemi - cezalog - afk - jail - ban - mute\`
 Veri depolaması fazla artarsa silineceketir!`)
