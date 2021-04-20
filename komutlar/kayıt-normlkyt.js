892a88ace8a47ca934096b2224f0a851927bff90
Glitch (hello-webpage) <none>
2021-04-20 00:04:27 +0000

    🐄🏡 Checkpoint ./komutlar/kayıt-sistemi.js:15334932/721 ./komutlar/kayıt-normal.js:15334932/5959 ./komutlar/kayıt-normlkyt.js:15334932/2163

---
+++ komutlar/kayıt-normlkyt.js
@@ -0,0 +1,53 @@
+const discord = require('discord.js')
+const db = require('quick.db')
+
+exports.run = async(client, message, args) => {
+
+  if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(` Bu komutu kullanabilmek için \`yönetici\` yetkisine sahip olmalısın`);
+
+if(args[0] === "sıfırla") {
+const darkcodeee = new discord.MessageEmbed()
+.setAuthor(client.user.username, client.user.avatarURL)  
+.setTitle(`${client.user.username} - Normal Üye Rol Sıfırla `)
+.setColor('BLACK')
+.setDescription(`Sunucu İçin Ayarladığınız Normal Üye Rolü Başarıyla Sıfırlandı !`)
+.setThumbnail(client.user.avatarURL)
+.setFooter(`Komut ${message.author.tag} Tarafından Kullanıldı ! `)
+message.channel.send(darkcodeee)
+db.delete(`norml_${message.guild.id}`)
+return;
+}
+
+let rol = message.mentions.roles.first();   
+if (!rol) {
+  const darkcodee = new discord.MessageEmbed()
+.setAuthor(client.user.username, client.user.avatarURL)  
+.setTitle(`${client.user.username} - Normal Üye Rol Ayarla `)
+.setColor('BLACK')
+.setDescription(`Ayarlayacağınız Normal Üye Rolünü Belirtiniz ! `)
+.setThumbnail(client.user.avatarURL)
+.setFooter(`Komut ${message.author.tag} Tarafından Kullanıldı ! `)
+message.channel.send(darkcodee)
+}
+db.set(`norml_${message.guild.id}`, rol.id)
+const darkcode = new discord.MessageEmbed()
+.setAuthor(client.user.username, client.user.avatarURL)  
+.setTitle(`${client.user.username} - Normal Üye Rol Ayarlandı `)
+.setColor('BLACK')
+.setDescription(`Normal Üye Rolü Başarıyla ${rol} Olarak Ayarlandı ! `)
+.setThumbnail(client.user.avatarURL)
+.setFooter(`Komut ${message.author.tag} Tarafından Kullanıldı ! `)
+message.channel.send(darkcode)
+  
+}
+exports.conf = {
+  enabled: true,
+  guildonly: false,
+  aliases: ['normal-rolN'],
+  permlevel: 0
+}
+exports.help = {
+  name: 'normal-rol',
+  description: 'erkek rolünü ayarlar',
+  usage: '!erkek-rol @rol'
+}
--- komutlar/kayıt-sistemi.js
+++ komutlar/kayıt-sistemi.js
@@ -1,16 +1,18 @@
 const Discord = require('discord.js')
 const ayarlar = require('../ayarlar.json')
 
-exports.run = function(client, message, args) {}
+exports.run = function(client, message, msg, args) {}
 let embed = new Discord.MessageEmbed()
 .setTitle("Alvi - Kayıt")
 .setColor("RANDOM")
-.addField(`
+.setDescription(`
 👩‍💼 \`${ayarlar.prefix}kız | ${ayarlar.prefix}kız-kayıt | ${ayarlar.prefix}k\` Kız kaydı yapar.
 👩‍💼 \`${ayarlar.prefix}kız-rol\` Kız kaydı yapıldığında verilecek rol.
 👨‍💼 \`${ayarlar.prefix}erkek | ${ayarlar.prefix}erkek-kayıt | ${ayarlar.prefiz}e\` Erkek kaydı yapar.
-👨‍💼 \`${ayarlar.prefix}erkek-rol\` Erkek kayıt 
-`)
+👨‍💼 \`${ayarlar.prefix}erkek-rol\` Erkek kaydı yapıldığında verilecek rol.
+📋 \`${ayarlar.prefix}alınacak-rol\` Bir kayıt yapıldığında **@Kayıtsız , @Misafir** rollerini almasını sağlar.
+👤 \`${ayarlar.prefix}normal | ${ayarlar.prefix}normal-kayıt | ${ayarlar.prefix}nk\` Üye kaydı yapar.
+👤 \`${ayarlar.prefix}normal-rol\` Üye kaydı yapıldığında verilecek rol.`)message.channel.send(embed)
 
 exports.conf = {
   enabled: true,
