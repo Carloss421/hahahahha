64e9624a1049751970b0b24400b6d8ccc5455731
Glitch (hello-webpage) <none>
2021-04-21 06:18:17 +0000

    👲🛀 Checkpoint ./komutlar/görev-sistemi.js:15334932/116

--- komutlar/görev-sistemi.js
+++ komutlar/görev-sistemi.js
@@ -3,14 +3,14 @@ const ayarlar = require('../ayarlar.json');
 
 exports.run = function(client, message, args) {
 let embed = new Discord.MessageEmbed()
-.setTitle(`Alvi - Moderasyon`)
+.setTitle(`Alvi - Görev Sistemi`)
 .setColor("RANDOM")
 .setDescription(`
 \`${ayarlar.prefix}görevli ayarla\` Görev vericek rolü ayarlar.
 \`${ayarlar.prefix}görevlog\` Logların düşeceği kanalı ayarlar.
 \`${ayarlar.prefix}görev-ekle\` Belirtilen kullanıcıya görev ekler.
 \`${ayarlar.prefix}görev-sil\` Belirtilen miktarda görev siler.
-\`${ayarlar.prefix}\`
+\`${ayarlar.prefix}görev-sayısı\` Belirtilen kullanıcı'nın görev sayısını görürsünüz
 `)
 message.channel.send(embed)
 };
@@ -18,10 +18,10 @@ message.channel.send(embed)
 exports.conf = {
  enabled: true,
  guildOnly: false,
- aliases: ["yardım-mod"],
+ aliases: ["görev-sistemi"],
  permlevel: 0
 };
 
 exports.help = {
-    name: "yardım-moderasyon"
+    name: "yardım-görev"
 };
