520f8c18883c4b86ff86240025be237b0141565c
Glitch (hello-webpage) <none>
2021-04-21 06:41:10 +0000

    🏄🕵 Checkpoint ./komutlar/yardım-moderasyon.js:15334932/26 ./komutlar/yardım-moderasyon2.js:15334932/4379

---
+++ komutlar/yardım-moderasyon2.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+exports.run = function(client, message, args) {
+let embed = new Discord.MessageEmbed()
+.setTitle(`Alvi - Moderasyon2`)
+.setColor("RANDOM")
+.setDescription(`
+\`${ayarlar.prefix}çekiliş-sistemi\` Çekiliş komutlarını açar.
+\`${ayarlar.prefix}botkoruma (YAKINDA - PREMUIM)\` İzinsiz sunucuya bot giremez.
+\`${ayarlar.prefix}müzik-sistemi\` Müzik komutlarını açar.
+
+**müzik-sistemi**'nin içindeki volume komutunu Normal Kullanıcılar: %59'a kadar - Premium Kullanıcıları: %100'yüze kadar arttırabiliyorlar.
+`)
+message.channel.send(embed)
+};
+
+exports.conf = {
+ enabled: true,
+ guildOnly: false,
+ aliases: ["yardım-mod2"],
+ permlevel: 0
+};
+
+exports.help = {
+    name: "yardım-moderasyon2"
+};
