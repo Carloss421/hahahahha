c5e3692b26dc52086dac8b7e0c9052860bb5d6c5
Glitch (hello-webpage) <none>
2021-04-21 05:31:50 +0000

    💳🐀 Checkpoint ./komutlar/ekonomi-paraEKLE.js:15334932/20 ./komutlar/görev-grvEKLE.js:15334932/5774 ./komutlar/ban.js:15334932/1

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -13,7 +13,7 @@ exports.run = (client, message, args) => {
       let modlog = message.guild.channels.find('name', 'cezalog');
     if (!modlog) return message.reply('`cezalog` kanalını bulamıyorum. Ayarlamak için `a!cezalog #cezalog`');
  
-  if (message.mentions.users.size < 1) return message.reply('Banalamak İstediğiniz Kişiyi Etiketleyiniz');
+  if (message.mentions.users.size < 1) return message.reply('Banlamak İstediğiniz Kişiyi Etiketleyiniz');
   if (reason.length < 1) return message.reply('Sebeb belirtin');
   if (user.id === message.author.id) return message.reply('Kendini Banlayamazssın');
 
--- komutlar/görev-grvEKLE.js
+++ komutlar/görev-grvEKLE.js
@@ -2,45 +2,53 @@ const Discord = require("discord.js");
 const db = require('quick.db')
 const ayarlar = require("../ayarlar.json")
 module.exports.run = async (client, message, args) => {
-
-
+  
+  if(message.author.id !== ayarlar.ownerID)  {
+    const embed = new Discord.MessageEmbed()
+    .setDescription(`**:x: Bu Komut Yapımcıma Özeldir !**`)
+    .setColor('BLUE')
+    return message.channel.send(embed).then(msg=>msg.delete(3000));
+    }
+  cont
+  let görevADD = await db.fetch(`görevEKLE_${message.guild.id}`)
+  
   let user = message.mentions.users.first()
 let money = args[1]    
-  if(message.author.id !== ayarlar.ownerID) return message.react("❌")
+  if(message.author.id !== ayarlar.botsahibi) return message.react("❌")
   if(!user) return message.channel.send(new Discord.MessageEmbed()
 .setColor("RED")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
-.setDescription(`Cüzdanına para eklenecek kişiyi etiketlemelisin!`))
+.setDescription(`Görev eklenicek kullanıcıyı etiketle!`))
 
   if(!args[1]) return message.channel.send(new Discord.MessageEmbed()
 .setColor("RED")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
-.setDescription(`⛔ Cüzdana eklenecek para miktarını girmelisin!`)) 
+.setDescription(`⛔ Görev adını girmelisin!`)) 
 
   if(isNaN(args[1])) return message.channel.send(new Discord.MessageEmbed()
 .setColor("RED")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
-.setDescription(`⛔ Cüzdana eklenecek para miktarını girmelisin!`)) 
+.setDescription(`⛔ Görev açıklmasını girmelisin`)) 
 
   if(args[1] < 0) return message.channel.send(new Discord.MessageEmbed()
 .setColor("RED")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
-.setDescription(`🤔 Girdiğin miktar geçerli bir sayı değil !?`));    
+.setDescription(`🤔 Göre Eklenemedi Galiba Hata Yaptın!`));    
 
+  
 
 message.channel.send(new Discord.MessageEmbed()
 .setColor("GREEN")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(`✅ ${user} kullanıcısının cüzdanına ${money} 💸 eklendi!`))
-db.add(`para_${user.id}`, money)  
+db.add(`görevEKLE_${user.id}`, money)  
 }
   
 exports.conf = {
   enabled: true,
-  aliases: ["para-ekle"],
+  aliases: ["görev-ekle"],
 };
 
 exports.help = {
-  name: 'Para Ekle',
+  name: 'görevle',
 };
- 
