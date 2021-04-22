da9c8d2c3897274e2ce208cb32cdb66fa16b67d5
Glitch (alvibotaltyapi) <none>
2021-04-22 20:44:17 +0000

    🏙🐬 Checkpoint ./events/message.js:15334932/1388

--- events/message.js
+++ events/message.js
@@ -44,8 +44,30 @@ if(message.author.bot) return
   let client = message.client;
   let prefix = await db.fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
   if (message.author.bot) return;
-  if (!message.content.starsWith(prefix))
-}
+  if (!message.content.startsWith(prefix)) return;
+  let command = message.content.split(' ')[0].slice(prefix.length);
+  let params = message.content.split(' ').slice(1);
+  let perms = client.elevation(message);
+  let cmd;
+  if (client.commands.has(command)) {
+    cmd = client.commands.get(command);
+    } else if (client.aliases.has(command)) {
+      cmd = client.commands.get(client.aliases.get(command));
+    }
+  if (cmd) {
+    let karaliste = await db.fetch('karalist')
+      if(message.author.id !== ayarlar.ownerID)
+  if(message.author.id !== ayarlar.ownerİD){
+       if(karaliste){
+  return message.channel.send(new Discord.MessageEmbed().setDescription(`
+  <@${message.author.id}>
+  **:warning: Aov sen karalistedesin komutlarımı kullanamazsın.
+  ❓ Karalisteye Alınma Sebebin: \`${karaliste}\`**`).setColor("RANDOM"))
+}}
+    if (perms < cmd.conf.permLevel) return;
+    cmd.run(client, message, params, perms);
+  }
+};
 /*
 const ayarlar = require('../ayarlar.json');
 const db = require('quick.db');
