50ad702f5985c53a9df1b6a2ae56c0af801f93f4
Glitch (alvibotaltyapi) <none>
2021-04-24 19:25:59 +0000

    🍳🐣 Checkpoint ./komutlar/canlıdestek.js:15334932/5506

--- komutlar/canlıdestek.js
+++ komutlar/canlıdestek.js
@@ -1,82 +1,39 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 
+const db = require("quick.db");
 
-exports.run = async(client, message, args) => {
+module.exports.run = async (bot, message, args,params,client) => {
+
+let prefix = "a!"
+    if(message.channel.type == "dm")  return;
+  if(message.channel.type !== "text") return;
+  var channel = client.channels.find('id', '678500000434487307')
+    const asdf = await client.channels.get(message.channel.id).createInvite()
+  message.delete();
+  const embed = new Discord.MessageEmbed()
+    .setAuthor(`${client.user.username}`, client.user.avatarURL)
+  .setTitle("»  Bot | Canlı Destek")
+  .setDescription("**<a:acik:827618729193242634> Canlı Desteği kullandığın için teşekkür ederiz, Seninle ekibim ilgilenicektir lütfen bekle!**")
+  .setDescription("30 Saniye İinde Geri Dönülmezse Lütfen İletişime Geçin \nCanlı Destek Ekibimiz <@739411430171738142>`-`<@720236094792400987>")
+  .setColor("#31ff00")
+ message.channel.send(embed)
+      const invite = new Discord.MessageEmbed()
+  .setAuthor("Canlı Destek | Talep")
+  .addField('**Kullanıcı: **', message.author.username + '#' + message.author.discriminator)
+  .addField('**Sunucu Adı: **', message.guild.name)
+  .setDescription(asdf.url)
+      channel.send(invite)
+};
   
-  
-const emoji1 = message.client.emojis.get('📞');
-const emoji2 = message.client.emojis.get('📞');
-const emoji3 = message.client.emojis.get('📞');
-const emoji4 = message.client.emojis.get('📞');
-const emoji5 = message.client.emojis.get('📞');
-const emoji6 = message.client.emojis.get('📞');
-const emoji7 = message.client.emojis.get('📞');
-      let isEnabled;
-      message.reply("Canlı Destek Komutunu Kullandığınız İçin Teşekkürler. Birazdan Yetkili Ekibimiz sizinle ilgilenicektir.");
-      let mesaj = args.slice(0).join(' ');
-      let chan = message.channel;
-      let destekKanal = "833215162047135744";
-      const embed = new Discord.MessageEmbed()
-        .addField('Uyarı', `📞 Canlı Destek Çağrısı`)
-        .setAuthor(`${message.author.tag} (${message.author.id})`, `${message.author.avatarURL}`)
-        .setColor("RANDOM")
-        .addField(`Bilgiler`, `**Sunucu**: ${message.guild.name} (${message.guild.id}) \n**Kanal**: ${message.channel.name} (${message.channel.id}) \n**Destek İsteyen**: ${message.author.tag} (${message.author.id}) \n**Destek Mesajı**: ${mesaj}`)
-        .setFooter("Canlı Destek")
-        .setTimestamp()
-      client.channels.get(destekKanal).send({
-        embed: embed
-      });
-    const collector = client.channels.get(destekKanal).createCollector(message => message.content.startsWith(''), {
-      time: 0
-    })
-    client.channels.get(destekKanal).send('📞 Destek çagrısı bağlanmak için `katıl` yazınız. İptal Etmek İçin `kapat` yazınız.')
-    collector.on('message', (message) => {
-      if (message.content === 'kapat') collector.stop('aborted')
-      if (message.content === 'katıl') collector.stop('success')
-    })
-    collector.on('end', (collected, reason) => {
-      if (reason === 'time') return message.reply('📞 Çagrı zaman aşımına uğradı.')
-      if (reason === 'aborted') {
-        message.reply('Çağrı reddedildi.')
-        client.channels.get(destekKanal).send('📞 Başarıyla çağrı reddedildi.')
-      }
-      if (reason === 'success') {
-        client.channels.get(destekKanal).send('📞 Destek çagrısı alındı!')
-        client.channels.get(destekKanal).send('📞 Destek çağrısını kapatmak için `kapat` yazınız.')
-        chan.send(`${message.author}`)
-        chan.send('Çağrınız bir destek yetkili tarafından alındı!')
-        chan.send('En Yakın Zamanda Size Yardımcı Olacagız.')
-        chan.send('Destek çagrısı kapatmak için `kapat` yazınız.')
-        isEnabled = true
-        client.on('message', message => {
-          function contact() {
-            if (isEnabled === false) return
-            if (message.author.id === client.user.id) return
-            if (message.content.startsWith('kapat')) {
-              message.channel.send('📞 Çağrı Kapatıldı.')
-              if (message.channel.id === chan.id) client.channels.get(destekKanal).send('📞 Çağrı karşı taraftan kapatıldı.')
-              if (message.channel.id === destekKanal) chan.send('📞 Çağrı karşı taraftan kapatıldı.')
-
-              return isEnabled = false
-            }
-            if (message.channel.id === chan.id) client.channels.get(destekKanal).send(`📞 **${message.author.tag}**: ${message.content}`)
-            if (message.channel.id === destekKanal) chan.send(`📞 **${message.author.tag}**: ${message.content}`)
-          }
-          contact(client)
-        })
-      }
-    })
-}
-
   exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["canlı-destek"],
-  permLevel: 0
+  aliases: ['canlıdestek'],
+  permLevel: 1
 };
 
 exports.help = {
-  name: 'canlıdestek',
+  name: 'canlı-destek',
   description: 'Canlı Destek Tablebi Oluşturur.',
-  usage: 'canlıdestek'
+  usage: 'c!canlı-destek'
 };
