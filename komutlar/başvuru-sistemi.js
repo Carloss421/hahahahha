afe180568c0c94243f5a586a83f0895012c00349
Glitch (alvibotaltyapi) <none>
2021-04-24 15:03:46 +0000

    🍏🌆 Checkpoint ./komutlar/başvuru.js:15334932/1484 ./komutlar/başvuru-sistemi.js:15334932/394

---
+++ komutlar/başvuru-sistemi.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+const ayarlar = require('../ayarlar.json')
+
+exports.run = function(msg, message) {
+let embed = new Discord.Messag
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["yardım-başvuru"],
+  permlevel: 0
+};
+exports.help = {
+  name: "başvuru-sistemi"
+};
---
+++ komutlar/başvuru.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+  let reason = args.slice(1).join(' ');
+  let guild = message.guild
+  let terfiler = message.guild.channels.cache.find(x => x.name === "başvurulog")  ;
+  if (!terfiler) return message.reply('`başvurulog` kanalını bulamıyorum. ');
+  let user = message.mentions.users.first();
+  if (reason.length < 1) return message.reply(new Discord.MessageEmbed().setDescription(
+    `**Ne Kazandirirsiniz:**
+    **Istediniz Yetki:**
+    **Kac Saat Aktif Olacaginiz:**
+    **Adınız:**
+    **Yaşınız ve Kendinizi Etiketleyin: **`));
+  if (message.mentions.users.size < 1) return message.reply('Isminizi Etiketleyin.').catch(console.error);
+  const embed = new Discord.MessageEmbed()
+    .setColor(0xD97634)
+	.setThumbnail("https://i.hizliresim.com/mJ20o2.jpg")
+    .setTimestamp()
+    .addField('Durum:', 'Beklemede')
+    .addField('Gonderen Kisi:', `${user.username}#${user.discriminator} (${user.id})`)
+    .addField('Bilgiler', reason);
+	
+	return guild.channels.cache.get(terfiler.id).sendEmbed(embed);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['başvur'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'başvuru-yap',
+  description: 'Kullanıcıyı terfi ettirir.',
+  usage: 'başvuru [kullanıcı]'
+};
