1444984c3a4d45dd4a663ab3eaa5a46273a68ed6
Glitch (alvibotaltyapi) <none>
2021-04-24 21:34:48 +0000

    👰🎒 Checkpoint ./komutlar/kayıt-kontrol.js:15334932/497

--- komutlar/kayıt-kontrol.js
+++ komutlar/kayıt-kontrol.js
@@ -8,15 +8,16 @@ let kayıter = db.fetch(`kayıte.${message.guild.id}`)
 let kayıtye = db.fetch(`kayıty.${message.guild.id}`)
 let kayıtlg = db.fetch(`logkayıt.${message.guild.id}`)
 let normalr = db.fetch(`norml_${message.guild.id}`)
+let alnck = db.fetch(`kayıtalınacakrol.${message.guild.id}`)
 let embed = new Discord.MessageEmbed()
 .setTitle("Alvi - Kayıt")
 .setColor("RANDOM")
-.setDescription(`
-**Kız Rolü:** ${db.has(`kayıtkadın.${message.guild.id}`) ? `<@&kayıtkadn>` : `\`Ayarlanmamış!\``}
-**Erkek Rolü:** ${db.has(`kayıte.${message.guild.id}`) ? `<@&kayıter>` : `\`Ayarlanmamış!\``}
-**Normal Rolü:** ${db.has(`norml_${message.guild.id}`) ? `<@&norml>` : `\`Ayarlanmamış!\``} 
-**Kayıt Yetkili Rolü:** ${db.has(`kayıty.${message.guild.id}` ? `<@&kayıty>` : `\`Ayarlanmamış!\``)}
-**Kayıt Log** ${db.has(`logkayıt.${message.guild.id}`) ? `<#logkayıt>` : `\`Ayarlanmamış!\``}`)
+.addField(`**Alınacak Rol**`, `${db.has(`kayıtalınacakrol.${message.guild.id}`) ? `<@&${alnck}>` : `\`Ayarlanmamış!\``} `,true)
+.addField(`**Kız Rolü**`, `${db.has(`kayıtkadın.${message.guild.id}`) ? `<@&${kayıtkadn}>` : `\`Ayarlanmamış!\``} `,true)
+.addField(`**Erkek Rolü**`, `${db.has(`kayıte.${message.guild.id}`) ? `<@&${kayıter}>` : `\`Ayarlanmamış!\``} `,true)
+.addField(`**Normal Rolü**`, `${db.has(`norml_${message.guild.id}`) ? `<@&${normalr}>` : `\`Ayarlanmamış!\``} `,true)
+.addField(`**Kayıt Yetkili Rolü**`, `${db.has(`kayıty.${message.guild.id}` ? `<@&${kayıtye}>` : `\`Ayarlanmamış!\``)} `,true)
+.addField(`**Kayıt Log**`, `${db.has(`logkayıt.${message.guild.id}`) ? `<#${kayıtlg}>` : `\`Ayarlanmamış!\``} `,true)
 message.channel.send(embed)
 };
 
