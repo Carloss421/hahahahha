b65791592d454ceffaa588f2597dfd02d00f7a37
Glitch (alvibotaltyapi) <none>
2021-04-27 12:59:19 +0000

    Exporting to AliBerat01/alvibotAltyapisi ./bot.js:15334932/1802 ./ayarlar.json:15334932/435

--- bot.js
+++ bot.js
@@ -1246,29 +1246,5 @@ client.on("guildMemberAdd", async member => {
     }
   });
 });
-const Client = new Discord.Client()
-Client.on('ready', () => {
-    console.log(Client.user.tag); 
-    fs.readdir('./komutlar/', (error, files) => {
-        if (error) return console.log(error); 
-        files.forEach(file => { 
-            if (!file.endsWith('.js')) return; 
-            let fileProp = require('./komutlar/' + file);
-            Client.api.applications(Client.user.id).commands.post({
-                data: {
-                    name: fileProp.help.name, 
-                    description: fileProp.help.description, 
-                    options: fileProp.help.options
-                }
-            });
-            Client.ws.on('INTERACTION_CREATE', async interaction => { 
-                const command = interaction.data.name.toLowerCase(); 
-                const args = interaction.data.options; 
-                if (command == fileProp.help.name.toLowerCase()) fileProp.run(Client, interaction, args); 
-            });
-        });
-    });
-});
-
 
 client.login(ayarlar.token);
