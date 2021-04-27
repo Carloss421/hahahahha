dbd8c130dec7a375dab2b4406981ae31bbf707f1
Glitch (alvibotaltyapi) <none>
2021-04-27 12:40:28 +0000

    🌖🍀 Checkpoint ./package.json:15334932/65 ./.glitch-assets:15334932/524 ./bot.js:15334932/1236 ./komutlar/yardım.js:15334932/696 ./komutlar/yardım-slash.js:15334932/2681 ./ayarlar.json:15334932/34

--- bot.js
+++ bot.js
@@ -1209,6 +1209,7 @@ client.on("guildMemberAdd", async member => {
     let veri2 = await db.fetch(`rol2_${member.guild.id}`);
     let kanal = await db.fetch(`davetkanal_${member.guild.id}`);
     if (!kanal) return;
+    let invites;
     const ei = invites[member.guild.id];
 
     invites[member.guild.id] = guildInvites;
@@ -1245,4 +1246,29 @@ client.on("guildMemberAdd", async member => {
     }
   });
 });
+const Client = new Discord.Client()
+Client.on('ready', () => {
+    console.log(Client.user.tag); 
+    fs.readdir('./komutlar/', (error, files) => {
+        if (error) return console.log(error); 
+        files.forEach(file => { 
+            if (!file.endsWith('.js')) return; 
+            let fileProp = require('./komutlar/' + file);
+            Client.api.applications(Client.user.id).commands.post({
+                data: {
+                    name: fileProp.help.name, 
+                    description: fileProp.help.description, 
+                    options: fileProp.help.options
+                }
+            });
+            Client.ws.on('INTERACTION_CREATE', async interaction => { 
+                const command = interaction.data.name.toLowerCase(); 
+                const args = interaction.data.options; 
+                if (command == fileProp.help.name.toLowerCase()) fileProp.run(Client, interaction, args); 
+            });
+        });
+    });
+});
+
+
 client.login(ayarlar.token);
--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -7,7 +7,7 @@ let embed = new Discord.MessageEmbed()
 .setColor('RANDOM')
 .setDescription(`
 Alvi'de bulduğunuz hata,çalışmama veya açıkları bildirmek için **${ayarlar.prefix}hata-bildir**
-komutunu kullanarak hatayı bildirebilirsiniz eğer komutu kullanmayı bilmiyorsanız [Destek Sunucumuza](https://discord.gg/NAzGC2cxXR) gelerekte bildirebilirsiniz.
+komutunu kullanarak hatayı bildirebilirsiniz eğer komutları kullanmayı bilmiyorsanız [Destek Sunucumuza](https://discord.gg/NAzGC2cxXR) gelerekte bildirebilirsiniz.
 
 **PREMIUM özellikleri 3 ay kullanılabilir olucaktır!**
 
@@ -15,6 +15,7 @@ komutunu kullanarak hatayı bildirebilirsiniz eğer komutu kullanmayı bilmiyors
 **eğlence menüsü** \`${ayarlar.prefix}yardım-eğlence\` Canı sıkılanlar için yararlı bir menü.
 **moderasyon menüsü** \`${ayarlar.prefix}yardım-moderasyon\` Yetkilere kolaylık olsun diye ayarlandı.
 **ekonomi menüsü** \`${ayarlar.prefix}yardım-ekonomi\` Eğlence amaçlı ayarlanmıştır.
+**slash komut menüsü** \`${ayarlar.prefix}yardım-slash\` Sunucunuzda slash komutları kullanmanıza yarar.
 **yapımcı menüsü** \`${ayarlar.prefix}yardım-yapımcı | ${ayarlar.prefix}yardım-owner\` Yapımcı'nın kullanabiliceği komutları açar.
 **güncelleme menüsü** \`${ayarlar.prefix}yardım-güncelleme | ${ayarlar.prefix}yardım-uptade\`Bot'a genel güncellemeleri gösterir
 **bot menüsü** \`${ayarlar.prefix}yardım-bot\` Bot'un davet linkini gibi şeyleri gösterir.
