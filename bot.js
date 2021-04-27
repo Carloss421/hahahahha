cbaf5fa6b5e9c1ec5062d358137b4a7110ce7967
Glitch (alvibotaltyapi) <none>
2021-04-27 14:45:37 +0000

    🐯🌯 Checkpoint ./bot.js:15334932/96998

--- bot.js
+++ bot.js
@@ -1,7 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+    app.get(".",(request, response) => {
+      console.log(`BOT AKTIF!`);
+      response.sendStatus(200);
+    });
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://alvibotaltyapi.glitch.me/`);
+}, 280000);
+const Discord = require('discord.js');
+const client = new Discord.Client({ disableMentions: 'everyone' });
+const ayarlar = require('./ayarlar.json');
 const fs = require('fs');
 const moment = require('moment');
-const db = require('quick.db');
-const ayarlar = require('./ayarlar.json')
+const db = require('quick.db')
 const queue = new Map();
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
@@ -12,11 +25,8 @@ require('moment-duration-format');
 var prefix = ayarlar.prefix;
 
 const log = message => {
- client.unload = command => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
 };
-
-
-
 
 
 var oyun = [
@@ -25,11 +35,82 @@ var oyun = [
 `🤖 Botu eklemek için | a!yardım-bot`
 ]
 
-client.on("ready", () => {
- client.elevation = message => {
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
 };
 
-client.login(ayarlar.token);
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
 //     [-----------------> Afk <------------------]  \\
 client.on("message" , async msg => {
   if(msg.content.startsWith(ayarlar.prefix+"afk")) return;
@@ -1146,4 +1227,4 @@ client.on("guildMemberAdd", async member => {
   });
 });
 
-client.login(ayarlar.token);
+client.login(ayarlar.token)
