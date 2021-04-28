94922aa4b3b7f9ac2e0c88963cae93b75731fb18
Glitch (alvibotaltyapi) <none>
2021-04-28 07:10:00 +0000

    📡🏹 Checkpoint ./komutlar/ayarlar.js:15334932/1243

--- komutlar/ayarlar.js
+++ komutlar/ayarlar.js
@@ -1,13 +1,33 @@
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
+const db = require('quick.db')
 
 exports.run = function(client, message, args) {
+
+let sayaçrol = db.fetch(`sayac_${message.guild.id}`) ? `${sayaçrol}` : `\`Ayarlanmamış!\``
+let sayaçkanal = db.fetch(`sayack_${message.guild.id}`) ? `${sayaçkanal}` : `\`Ayarlanmamış!\``
+let otorol = db.fetch(`otoRL_${message.guild.id}`) ? `${otorol}` : `\`Ayarlanmamış!\``
+let otorolkanal = db.fetch(`otoRK_${message.guild.id}`) ? `${otorolkanal}` : `\`Ayarlanmamış!\``
+
+let kayıtlog = db.fetch(`logkayıt.${message.guild.id}`) ? `${kayıtlog}` : `\`Ayarlanmamış!\``
+//  db.set(`otoRL_${message.guild.id}`, rol.id);
+//  db.set(`otoRK_${message.guild.id}`, kanal.id);
+
 let embed = new Discord.MessageEmbed()
+
 .setTitle(`Alvi - Ayarlar`)
 .setColor("RANDOM")
 .setDescription(`
 **SAYAC**
-
+**Rol:** ${sayaçrol} **Kanal:** ${sayaçkanal}
+`,true)
+.setDescription(`
+**OTOROL**
+**Rol:** ${otorol} **Kanal:** ${otorolkanal}
+`,true)
+.setDescription(`
+**KAYIT-SISTEMI**
+**Rol:** **Kanal:** **Yetkili:** **Log:** ${kayıtlog}
 `)
 message.channel.send(embed)
 };
