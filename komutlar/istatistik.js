138072cf0a727febff151bb282be4e31b3e07117
Glitch (alvibotaltyapi) <none>
2021-04-30 18:18:30 +0000

    🛎🐚 Checkpoint ./komutlar/istatistik.js:15334932/271

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -18,16 +18,16 @@ Kuruluş tarihi ${araEmoji} **${ayarlar.krştrh}**
 Sunuluş tarihi ${araEmoji} **${ayarlar.snştrh}**
 
 Çalışma Süresi ${araEmoji} **${duration}**
-Bot Gecikmesi ${araEmoji} **${client.ws.ping}ms**
-Mesaj Gecikmesi ${araEmoji} **-${new Date().getTime() - message.createdTimestamp}ms**
+<a:discordLOGO:837753587483803698> Bot Gecikmesi ${araEmoji} **${client.ws.ping}ms**
+<a:discordLOGO:837753587483803698> Mesaj Gecikmesi ${araEmoji} **-${new Date().getTime() - message.createdTimestamp}ms**
 
 <:kullanici:837751329320468511> Toplam kullanıcı ${araEmoji} **${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}  [${message.client.users.cache.filter(m => m.bot).size} BOT]**
-Toplam sunucu ${araEmoji} **${client.guilds.cache.size}**
+<:developer:837752031346688001> Toplam sunucu ${araEmoji} **${client.guilds.cache.size}**
 <:kanal:837747364378181642> Toplam kanal ${araEmoji} **${client.channels.cache.size}**
-Toplam komut ${araEmoji} **${client.commands.size}**
+:keyboard: Toplam komut ${araEmoji} **${client.commands.size}**
 
 Müzik Oynatılan sunucu ${araEmoji} **${client.voice.connections.size}**
-Discord.js sürümü ${araEmoji} **v${Discord.version}**
+<:discordJS:837754572897910796> Discord.js sürümü ${araEmoji} **v${Discord.version}**
 Node.js sürümü ${araEmoji} **${process.version}**
 
 **Verileri alınan komutlar**
