19bee11cb78e38fae5859917f5600418be5c47ed
Glitch (alvibotaltyapi) <none>
2021-05-01 11:03:49 +0000

    🌅🛋 Checkpoint ./komutlar/yardım-premium.js:15334932/194

--- komutlar/yardım-premium.js
+++ komutlar/yardım-premium.js
@@ -14,11 +14,17 @@ Premium özel komutları kullanabilir. Premium herşeyde öncelik sağlar,%38 da
 5 aylık premium \`45 TL\`
 10 aylık premium \`46 TL\`
 1.5 yıllık premium \`56 TL\`
+
 **Premium Özellikleri**
-\`1 aylık premium'da\` **bot'un istatistiklerinde premium kullanıcı (1 aylık) olarak isminiz yazılır ve eğlence,kullanıcı menülerindeki premium komutlarını kullanabilirsiniz.**
+
+\`1 aylık premium'da\` **bot'un istatistiklerinde premium kullanıcı (1 aylık) olarak isminiz yazılır ve eğlence,kullanıcı menülerindeki premium komutlarını kullanabilirsiniz ve sunucumuzda \`PREMIUM KULLANICI\` rolünü alabilirsiniz.**
+
 \`3 aylık premium'da\` **bot'un istatistiklerinde premium kullanıcı (3 aylık) olarak isminiz yazılır ve eğlence,kullanıcı,moderasyon menülerindeki premium komutlarını kullanabilirsiniz.**
-\`5 aylık premium'da\` **bot'un istatistiklerinde premium kullanıcı (5 aylık) olarak isminiz yazılır ve eğlence,kullanıcı,moderasyon menülerindeki premium komutlarını kullanabilirsiniz.**
-\`10 aylık premium'da\` **bot'un istatistiklerinde premium kullanıcı (10 aylık) olarak isminiz yazılır, sunucunuza özel komutlar ve eğlence,kullanıcı,moderasyon menülerindeki premium komutlarını kullanabilirsiniz.**
+
+\`5 aylık premium'da\` **bot'un istatistiklerinde premium kullanıcı (5 aylık) olarak isminiz yazılır ve eğlence,kullanıcı,moderasyon menülerindeki premium komutlarını kullanabilirsiniz ve sunucumuzda \`PREMIUM KULLANICI\` rolünü alabilirsiniz.**
+
+\`10 aylık premium'da\` **bot'un istatistiklerinde premium kullanıcı (10 aylık) olarak isminiz yazılır, sunucunuza özel komutlar ve eğlence,kullanıcı,moderasyon menülerindeki premium komutlarını kullanabilirsiniz ve sunucumuzda \`PREMIUM KULLANICI\` rolünü alabilirsiniz.**
+
 \`1.5 yıllık premium'Da\` **bot'un istatistiklerinde premium kullanıcı (1.5 yıllık) olarak isminiz yazılır, sunucununuza özel komutlar, eğlence,kullanıcı,moderasyon menülerindeki premium komutlarını kullanabilirsiniz ve sunucumuzda,
 \`PREMIUM KULLANICI\` rolünü alabilirsiniz.**
 `)
