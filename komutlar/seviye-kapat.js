2c9370063386b7fd5a7c7c0aba85c2f2a055ba6b
Glitch (alvibotaltyapi) <none>
2021-05-04 20:29:13 +0000

    🙅🍰 Checkpoint ./komutlar/seviye-kapat.js:15334932/1082

--- komutlar/seviye-kapat.js
+++ komutlar/seviye-kapat.js
@@ -1,32 +1,32 @@
 const Discord = require('discord.js');
-const db = require('quick.db')            //seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare
-exports.run = async(client, message, args) => {  //seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare
+const db = require('quick.db')
+const ayarlar = require('../ayarlar.json')
+exports.run = async(client, message, args) => {
 
  let hm = await db.fetch(`seviyeacik_${message.guild.id}`)
-  let kanal = await db.fetch(`svlog_${message.guild.id}`)                //seyit ve codare
+  let kanal = await db.fetch(`svlog_${message.guild.id}`)
   
-    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(` <a:dikkat:632947931808268290> Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(new Discord.MessageEmbed().setDescription(`${ayarlar.hata} Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`));
   
-  if(!hm) return message.channel.send('Seviye sistemi zaten aktif hale getirilmemiş!\n Bunu mu arıyorsun? `!seviye-aç`')                //seyit ve codare
+  if(!hm) return message.channel.send(new Discord.MessageEmbed().setDescription('Seviye sistemi zaten aktif hale getirilmemiş!\n Bunu mu arıyorsun? `a!seviye aç`'))
   
-  message.reply('Seviye sistemi devre dışı durumuna getiriliyor..').then(seyit => {                       //seyit ve codare
-    
+  message.reply(new Discord.MessageEmbed().setDescription('Seviye sistemi devre dışı durumuna getiriliyor..')).then(seyit => {
+  let evet = new Discord.MessageEmbed()
+.setDescription("Getirildi!")
+  message.channel.send(evet)
  db.delete(`seviyeacik_${message.guild.id}`)
-    //seyit ve codare
- db.delete(`svlog_${message.guild.id}`)//seyit ve codare                          //seyit ve codare
+ db.delete(`svlog_${message.guild.id}`)
 
   seyit.edit('sistem devre dışı bırakıldı!')  
-    
-  }, 5000)//seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare//seyit ve codare
-                            //seyit ve codare
+  }, 5000)
   
 
   
   };
 exports.conf = {
-  enabled: true,  
-  guildOnly: false,                   //seyit ve codare
-  aliases: [], 
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
   permLevel: 0
 };
 
