3c6c9dba85fc035251d8df15b9bc697d31b58e5a
Glitch (alvibotaltyapi) <none>
2021-05-04 17:06:28 +0000

    🌥🤗 Checkpoint ./komutlar/kayıt-systemRELOAD.js:15334932/5515 ./komutlar/kayıt-syistemRELOAD.js:15334932/6460

--- komutlar/kayıt-syistemRELOAD.js
+++ komutlar/kayıt-syistemRELOAD.js
@@ -1,11 +1,10 @@
 const Discord = require('discord.js')
 const ayarlar = require('../ayarlar.json')
 const db = require('quick.db')
-
-exports.run = function(client, msg, message, args) {
+exports.run = function(client, message, args) {
   
 
- let getirildi = new Discord.MessageEmbed()
+let getirildi = new Discord.MessageEmbed()
 .setDescription("Bütün herşey geri getirildi!")
 message.channel.send(getirildi)
 db.fetch(`knormalrol_${message.guild.id}`)
@@ -14,15 +13,14 @@ db.fetch(`kkayıtçırol_${message.guild.id}`)
 db.fetch(`kkayıtsayı_${message.guild.id}`)
 db.fetch(`kkayıtkanal_${message.guild.id}`)
 db.fetch(`kkızrol_${message.guild.id}`)
-db.fetch(`kkayıtlog_${message.guild.id}`) 
-  
+db.fetch(`kkayıtlog_${message.guild.id}`)
+
 };
-  
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["kayıtları getir"],
+   aliases: ["kayıtları getir"],
   permlevel: 0
 };
 
--- komutlar/kayıt-systemRELOAD.js
+++ komutlar/kayıt-systemRELOAD.js
@@ -1,10 +1,8 @@
 const Discord = require('discord.js')
 const ayarlar = require('../ayarlar.json')
 const db = require('quick.db')
-
-exports.run = function(client, msg, message, args) {
+exports.run = function(client, message, args) {
   
-
 let sıfırlandı = new Discord.MessageEmbed()
 .setDescription("Kayıt sistemi sıfırlandı eğer geri getirmek istiyorsan `a!kayıt-sistemini geri getir` Baştan ayarlanabilir.")
 message.channel.send(sıfırlandı)
@@ -16,20 +14,16 @@ db.delete(`kkayıtkanal_${message.guild.id}`)
 db.delete(`kkızrol_${message.guild.id}`)
 db.delete(`kkayıtlog_${message.guild.id}`)
 
-  
-  
-
-  
+message.channel.send(sıfırlandı)
 };
-  
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["kayıtları"],
+  aliases: ["kayıtları sıfırla"],
   permlevel: 0
 };
 
 exports.help = {
- name: "kayıt-sistemini sıfırla"
+ name: "kayıt-sistemini-sıfırla"
 };
