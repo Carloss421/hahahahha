8aef059611b1abc4c45f92de38cd5ac2786571fe
Glitch (alvibotaltyapi) <none>
2021-05-04 16:20:50 +0000

    📊🎢 Checkpoint ./komutlar/kayıt-systemRELOAD.js:15334932/2248 ./komutlar/kayıt-sistemi.js:15334932/134

--- komutlar/kayıt-systemRELOAD.js
+++ komutlar/kayıt-systemRELOAD.js
@@ -4,6 +4,32 @@ const db = require('quick.db')
 
 exports.run = function(client, msg, message, args) {
   
+if(args[0] === 'sıfırla') {
+let sıfırlandı = new Discord.MessageEmbed()
+.setDescription("Kayıt sistemi sıfırlandı eğer geri getirmek istiyorsan `a!kayıt-sistemi geri getir` Baştan ayarlanabilir.")
+message.channel.send(sıfırlandı)
+db.delete(`knormalrol_${message.guild.id}`)
+db.delete(`kerkekrol_${message.guild.id}`)
+db.delete(`kkayıtçırol_${message.guild.id}`)
+db.delete(`kkayıtsayı_${message.guild.id}`)
+db.delete(`kkayıtkanal_${message.guild.id}`)
+db.delete(`kkızrol_${message.guild.id}`)
+db.delete(`kkayıtlog_${message.guild.id}`)
+};
+  
+if(args[1] === 'geri getir') {
+let getirildi = new Discord.MessageEmbed()
+.setDescription("Bütün herşey geri getirildi!")
+message.channel.send(getirildi)
+db.fetch(`knormalrol_${message.guild.id}`)
+db.fetch(`kerkekrol_${message.guild.id}`)
+db.fetch(`kkayıtçırol_${message.guild.id}`)
+db.fetch(`kkayıtsayı_${message.guild.id}`)
+db.fetch(`kkayıtkanal_${message.guild.id}`)
+db.fetch(`kkızrol_${message.guild.id}`)
+db.fetch(`kkayıtlog_${message.guild.id}`)
+};
+  
 };
 
 exports.conf = {
@@ -14,5 +40,5 @@ exports.conf = {
 };
 
 exports.help = {
-  
+ name: "kayıt-sistemi"
 };
