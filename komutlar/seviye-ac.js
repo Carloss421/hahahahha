4d1e9aa5679063456aad5af6889710a037b8c897
Glitch (alvibotaltyapi) <none>
2021-05-04 20:06:41 +0000

    🐿🍟 Checkpoint ./komutlar/seviye.js:15334932/16765 ./komutlar/seviye-kanal.js:15334932/2646 ./komutlar/seviye-ac.js:15334932/138 ./ayarlar.json:15334932/271

---
+++ komutlar/seviye-ac.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const db = require('quick.db')
+const ayarlar = require('../ayarlar.json')
+exports.run = async(client, message, args) => { 
+  
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`${ayarlar.oldu} Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
+  
+ 
+  let hm = await db.fetch(`seviyeacik_${message.guild.id}`)
+  
+  if(hm) return message.reply('Bu tuhaf! Anlaşılan seviye sistemi zaten aktif edilmiş.. \n Bunu mu arıyorsun? `!seviye-kapat`')
+  
+  
+  
+  let kanal = await db.fetch(`svlog_${message.guild.id}`)
+  
+  
+  let kontrol;
+  if(kanal == null) kontrol = 'Sunucuda Ayarlanmış Bir Logs Bulunamadı!'
+  else kontrol = kanal
+  
+  
+  
+  
+  let seviyn = new Discord.MessageEmbed()
+  .setTitle('Aktif Edildi!')
+  .setDescription(message.guild.name + ' Sunucusuna başarıyla seviye sistemini aktifleştirdim!\n Genel ayarlar aşağıda veriliyor..')
+  .addField('Seviye Log Kanalı:', kontrol, true)
+  .setColor('RANDOM')
+  message.channel.send(seviyn)
+ 
+message.guild.owner.send('Seviye sistemi **'+message.member.user.username+'** ('+message.member.id+') tarafından aktifleştirildi!\n `Seviye Sistemi`')
+  
+  
+db.set(`seviyeacik_${message.guild.id}`, 'açık')
+
+};
+exports.conf = {
+  enabled: true,  
+  guildOnly: false, 
+  aliases: [], 
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'seviye-aç',
+  description: 'taslak', 
+  usage: 'seviye-aç'
+};
