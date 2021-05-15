d3a00a9643861a2fd43684a010a69e73be263c4e
Glitch (alvibotaltyapi) <none>
2021-05-15 18:11:34 +0000

    🤘👛 Checkpoint ./webpanel/html/anasayfa.ejs:15334932/480 ./webpanel/html/bas.ejs:15334932/5258 ./webpanel/html/başlık.ejs:15334932/2207 ./webpanel/html/doğrulama.ejs:15334932/3708 ./webpanel/html/hakkimda.ejs:15334932/1008 ./webpanel/html/iletisim.ejs:15334932/1131 ./webpanel/html/istatistikler.ejs:15334932/3121 ./webpanel/html/k-panel.js:15334932/106421 ./webpanel/html/kanallar.ejs:15334932/5536 ./webpanel/html/komutlar.ejs:15334932/16787

--- webpanel/html/k-panel.js
+++ webpanel/html/k-panel.js
@@ -1,27 +1,25 @@
 <!doctype html>
 <html lang="en">
-  <% const db = bot.ayar; %>
+<%- include('başlık.ejs', {bot, user, path}) %>
 <head>
 <% if (bot.ayar.has(`karalist_${user.id}`)) return "Botun komutlarını kullanabilme ve siteyi görme hakkınız bulunmamaktadır!";  { %>
  <% } %>
- <% db.set(`karalist_${user.id}`,'webp')  %>
- <% db.delete(`karalist_${user.id}`)  %>
-
+ 
+  <!-- Required meta tags -->
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
- 
+  <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-
+  <!-- Material Kit CSS -->
 
 <title>
   Merhaba, <%=user.username%>
   </title>
-     
    <link href="/css/site.css" rel="stylesheet" />
-    <link href="./css/demo.css" rel="stylesheet" />
+    <link href="/css/demo.css" rel="stylesheet" />
   <link rel='icon' href='<%=bot.avatarURL%>'>
 
 
@@ -29,45 +27,55 @@
 
 <body class="dark-edition">
   <div class="wrapper ">
-    <div class="sidebar" data-color="danger" data-background-color="black">
-    
-          <div class="logo"><a href="https://alvibotaltyapi.glitch.me/Anasayfa" class="simple-text logo-normal">
-          Alvi Anasayfa
+    <div class="sidebar" data-color="mor" data-background-color="black" data-image="./css/olsana.png">
+      <!--
+        Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+        Tip 2: you can also add an image using data-image tag
+    -->
+      <div class="logo"><a href="https://dash.vortexbot.org/anasayfa" class="simple-text logo-normal">
+          Vortex Anasayfa
         </a></div>
       <div class="sidebar-wrapper">
         <ul class="nav">
-          <li class="nav-item active  ">
-            <a class="nav-link" href="https://alvibotaltyapi.glitch.me/Anasayfa">
+        
+         <li class="nav-item ">
+            <a class="nav-link" href="http://dash.vortexbot.org/anasayfa">
               <i class="material-icons">announcement</i>
               <p>Bilgilendirme</p>
             </a>
           </li>
           <li class="nav-item ">
-           <a class="nav-link" href="https://alvibotaltyapi.glitch.me/panel">
+           <a class="nav-link" href="http://dash.vortexbot.org/panel">
               <i class="material-icons">dashboard</i>
               <p>Yönetim Paneli</p>
             </a>
           </li>
- 
+            <li class="nav-item active  ">
+            <a class="nav-link" href="http://dash.vortexbot.org/kullanici/<%=user.id%>/yonet">
+              <i class="material-icons">photo</i>
+              <p>Seviye Kartı</p>
+            </a>
+          </li>
           <li class="nav-item ">
-            <a class="nav-link" href="https://alvibotaltyapi.glitch.me/kullanici/<%=user.id%>">
+            <a class="nav-link" href="http://dash.vortexbot.org/kullanici/<%=user.id%>">
               <i class="material-icons">person</i>
               <p>Kullanıcı Bilgi</p>
             </a>
           </li>
            <li class="nav-item ">
-            <a class="nav-link" href="https://vortex-altyaps.glitch.me/komutlar">
+            <a class="nav-link" href="http://dash.vortexbot.org/komutlar">
               <i class="material-icons">dvr</i>
               <p>Komutlar</p>
             </a>
           </li>
           
-          
-              <li class="nav-item ">
-            <a class="nav-link" target="_blank" href="https://alvibotaltyapi.glitch.me/botuekle">
+         
+          <li class="nav-item ">
+            <a class="nav-link" href="http://dash.vortexbot.org/botuekle">
               <i class="material-icons">
 add</i>
-              <p>Alvi'yi Sunucuna Ekle</p>
+              <p>Botu Sunucuna Ekle</p>
               </a>
           </li>
                         <li class="nav-item ">
@@ -81,15 +89,28 @@ add</i>
       </div>
     </div>
     <div class="main-panel">
-     
+      <!-- Navbar -->
       <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top " id="navigation-example">
         <div class="container-fluid">
           <div class="navbar-wrapper">
             <a class="navbar-brand" href="javascript:void(0)">Dashboard</a>
           </div>
-
-    
-          
+        <%if(user) {%>
+               <%  if(bot.ayar.has(`üyelikk_${user.id}`) === true)  { %>
+            <div data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
+              <img src="https://cdn.discordapp.com/avatars/<%=user.id%>/<%=user.avatar%>.png?size=32" width="31" height="31" class="d-inline-block align-middle rounded-circle" style="border-style:solid;border-width: 1px;border-color: #eef60e ">
+           </a>
+          <%  }  %> 
+         
+      <%}%>
+             <%if(user) {%>
+               <%  if(!bot.ayar.has(`üyelikk_${user.id}`) === true)  { %>
+            <div data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
+              <img src="https://cdn.discordapp.com/avatars/<%=user.id%>/<%=user.avatar%>.png?size=32" width="30" height="30" class="d-inline-block align-middle rounded-circle" alt="">
+           </a>
+          <%  }  %> 
+         
+      <%}%>
           
              <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation" data-target="#navigation-example">
             <span class="sr-only">Toggle navigation</span>
@@ -104,165 +125,116 @@ add</i>
            
       
           
-         
-        
- 
-      <div class="content">
-        <div class="container-fluid">
-       Selam, <%=user.username%> Alvi'nin Web Panel'ine Hoş Geldiniz.
-          
+  <% const db = bot.ayar; %>
+<% const u = bot.users.get(user.id); %>
 
+<div class="container">
+	
+		<div align="center">
+<form method="POST">
 
-         
-    
-        </div>
-        <br>
-        <br>
-       
-        
-      <div class="row">
-            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-              <div class="card card-stats">
-                <div class="card-header card-header-warning card-header-icon">
-                  <div class="card-icon">
-                    <i class="material-icons">person</i>
-                  </div>
-                  <p class="card-category">Profil</p>
-                  <h3 class="card-title"><small><h5>
+<h3 id="sssss"></h3>
+  <br>
+<h2>
+  Seviye Kartı Düzenleme 
   
-                   <%  if(bot.ayar.has(`üyelikk_${user.id}`) === true)  { %>Üyelik: Gold Üye<%  }  %> 
-                   <%  if(!bot.ayar.has(`üyelikk_${user.id}`) === true)  { %>Üyelik: Normal Üye<%  }  %> 
-                               </h5>                          
-                          </small>
-                    
-                  </h3>
+  </h2>
+
+<br>
+  <br>
+
+  
+
+    <div class="row">
+      <div class="col-25">
+          <label for="lvl2">Seviye sistemi</label>
+      </div>
+      <div class="col-75">
+        <% if (bot.ayar.has(`lvl2_${user.id}`) === false) { %>
+        <label style="margin-right:80%;" class="switch">
+         <input type="checkbox" id="lvl2" name="lvl2" value="aktif">
+         <span class="slider round"></span>
+        </label>
+          <% } %>
+					<% if(bot.ayar.has(`lvl2_${user.id}`) === true) { %>
+        <label style="margin-right:80%;" class="switch">
+         <input type="checkbox" id="lvl2" name="lvl2" value="deaktif" checked>
+         <span class="slider round"></span>
+        </label>
+          <% } %>
+      </div>
+    </div>
+<div class="row">
+<div class="col-25">
+<label for="renk">Seviye kartı rengi</label>
+</div>
+<div class="col-75">
+<% if (bot.ayar.has(`${user.id}.renk`) === false) { %>
+<input type="color" id="renk" name="renk" style="width:87%" value="#84a0ed">
+<a href="/kullanici/<%=user.id%>/yonet/renk/sifirla" class="reset-btn" role="button">Sıfırla</a>
+<% } else { %>
+<input type="color" id="renk" name="renk" style="width:87%" value="<%=bot.ayar.fetch(`${user.id}.renk`)%>">
+<a href="/kullanici/<%=user.id%>/yonet/renk/sifirla" class="reset-btn" role="button">Sıfırla</a>
+<% } %>
+</div>
+</div>
+
+
+<div class="row">
+<div class="col-25">
+<label for="resim">Seviye kartı arka plan resmi</label>
+</div>
+
+<div class="col-75">
+<% if (bot.ayar.has(`${user.id}.resim`) === false || bot.ayar.fetch(`${user.id}.resim`) === "https://img.revabot.tk/99kd63vyss.png") { %>
+<input type="url" id="resim" name="resim" style="width:87%" placeholder="Bir resim URL'si yazınız.">
+<a href="/kullanici/<%=user.id%>/yonet/resim/sifirla" class="reset-btn" role="button">Sıfırla</a>
+<% } else { %>
+<input type="url" id="resim" name="resim" style="width:87%" placeholder="Bir resim URL'si yazınız resim .png veya .jpg şeklinde olmalıdır ve boyutu çok büyük olmamalıdır." value="<%=bot.ayar.fetch(`${user.id}.resim`)%>">
+<a href="/kullanici/<%=user.id%>/yonet/resim/sifirla" class="reset-btn" role="button">Sıfırla</a>
+<div align="left" class="col-25">
+  
+
+
+<label for="resim">Resim</label>
+ 
+  
+<img src="<%=bot.ayar.fetch(`${user.id}.resim`)%>" width="600px" height="450px"><br><br>
     
-                </div>
-                <div class="card-footer">
-                  <div class="stats">
-                
-                    
-                    
-                 Alvi Dashboard
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-              <div class="card card-stats">
-                <div class="card-header card-header-success card-header-icon">
-                  <div class="card-icon">
-                    <i class="material-icons">all_inclusive</i>
-                  </div>
-                  <p class="card-category">Botun Bulunduğu Sunucu Sayısı</p>
-                  <h3 class="card-title"><%=bot.guilds.size%></h3>
-                </div>
-                <div class="card-footer">
-                  <div class="stats">
-                 Alvi Dashboard
-                  </div>
-                </div>
-              </div>
-            </div>
-    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-              <div class="card card-stats">
-                <div class="card-header card-header-danger card-header-icon">
-                  <div class="card-icon">
-                    <i class="material-icons">group</i>
-                  </div>
-                  <p class="card-category">Kullanıcı Sayısı</p>
-                  <h3 class="card-title"><%=bot.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()%></h3>
-                  <br>
-                </div>
-                
-                <div class="card-footer">
-                  <div class="stats">
-                   Alvi Dashboard
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-              <div class="card card-stats">
-                <div class="card-header card-header-info card-header-icon">
-                  <div class="card-icon">
-                    <i class="material-icons">cloud</i>
-                  </div>
-                  <p class="card-category">Gecikme Süresi</p>
-                  <h3 class="card-title"><%=Math.round(bot.ping)%> MS</h3>
-                  <br>
-                  
-                </div>
-                <div class="card-footer">
-                  <div class="stats">
-                Alvi Dashboard
-                  </div>
-                </div>
-              </div>
-            </div>
-       <!-- Merhabalar. Ben yusuf yaklaşık 3 yıldır uğraştığım bot kodlama ile ilgili merağım sona erdi ve bu botu size hediye ediyorum.
-Botta hiç bir sıkıntı yok çoğu kodlar benim tarafımdan fakat bazı kodlar ise alıntıdır. (10 Kod civarı)
-Token girip site işlemlerinizi hallettikten sonra bot & sitesi açılacaktır.
-Unutmayın vortex sıradan bir bot değildir VIP bot olan vortexi sizlere armağan ediyorum
-Bu iyliğimi unutmayın hata olursa Cod are daha detaylı zor hata olursa Yu
-        
-          </div>
-         <div class="row">
-            <div class="col-lg-6 col-md-12">
-              <div class="card">
-                <div class="card-header card-header-primary">
-                  <h4 class="card-title">Linkler</h4>
-                  <p class="card-category">Vortex Dashboard Sistemi</p>
-                </div>
-                <div class="card-body table-responsive">
-                  <table class="table table-hover">
-                    <thead class="text-warning">
-                      <th>İsim</th>
-                      <th>Görev</th>
-                      
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>Destek Sunucusu</td>
-                        
-                        <td> <a href="https://discord.gg/knESwdy" target="_blank" rel="noopener noreferrer">Tıkla!</a></td>
-                        
-                      </tr>
-                      <tr>
-                        <td>Bot List</td>
-                        <td> <a href="https://top.gg/bot/612726771632308225" target="_blank">Tıkla!</a></td>
-                    
-                      </tr>
-                  
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-           
+
+</div>
+<% } %>
+</div>
+</div>
+
+<button id="formbtn" class="save-btn" type="submit"><i class="fa fa-save fa-fw"></i> Değişiklikleri Kaydet</button>
+
+      </form>
     </div>
   </div>
+  </div>
+    
   
   <script src="/css/js/jquery.min.js"></script>
   <script src="/css/js/popper.min.js"></script>
   <script src="/css/js/bootstrap-material-design.min.js"></script>
   <script src="https://unpkg.com/default-passive-events"></script>
   <script src="/css/js/perfect-scrollbar.jquery.min.js"></script>
-
+  <!-- Place this tag in your head or just before your close body tag. -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!--  Google Maps Plugin    -->
 
-
-
+  <!-- Chartist JS -->
   <script src="/css/js/chartist.min.js"></script>
-
+  <!--  Notifications Plugin    -->
   <script src="/css/js/bootstrap-notify.js"></script>
-
+  <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="/css/js/material-dashboard.js?v=2.1.0"></script>
     
- 
+  <!-- Material Dashboard DEMO methods, don't include it in your project! -->
   <script src="/css/js/demo.js"></script>
 
-
+ <!--   Core JS Files   -->
   
   <script>
     $(document).ready(function() {
