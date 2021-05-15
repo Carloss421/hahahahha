146d98dcbfcb1b65ce3349edfe7c8ce484cf275e
Glitch (alvibotaltyapi) <none>
2021-05-15 00:00:04 +0000

    🚍🚈 Checkpoint ./events/ready.js:15334932/3051 ./util/eventLoader.js:15334932/368

--- events/ready.js
+++ events/ready.js
@@ -6,9 +6,32 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-console.log(`BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Alvi Aktif, Komutlar yüklendi!`);
+  var oyun = [ 
+"🎀 Yardım almak için | a!yardım",
+"🔔 Yeni Özellikler İçin | a!yardım-güncelleme",
+"🤖 Botu eklemek için | a!yardım-bot"
+  
+    ];
+client.user.setActivity("online")
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setWatch(oyun[random]);
+        }, 2 * 6000);
+}
+/*const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Alvi Aktif, Komutlar yüklendi!`);
+
  client.user.setActivity("online");
   var oyun = [ 
 `🎀 Yardım almak için | a!yardım`,
@@ -23,4 +46,4 @@ console.log(`BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.gu
 
         client.user.setWatch(oyun[random], "https://www.twitch.tv/asreaper13");
         }, 2 * 18000);
-}
+}*/
