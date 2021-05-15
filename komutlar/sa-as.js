49d6998187b6839a9b4e5428ad555b3a6aacab83
Glitch (alvibotaltyapi) <none>
2021-05-15 21:12:37 +0000

    🐢🐝 Checkpoint ./komutlar/sa-as.js:15334932/133

--- komutlar/sa-as.js
+++ komutlar/sa-as.js
@@ -4,16 +4,16 @@ const Discord = require('discord.js')
 exports.run = async (bot, message, args) => {
 const ayarlar = require('../ayarlar.json')
 let prefix = db.fetch(`prefix_${message.guild.id}`) || ayarlar.prefix;
-  if (!args[0]) return message.channel.send('Sa-as yazısını açmak için; `'+ prefix +'sa-as aç veya kapat`')
+  if (!args[0]) return message.channel.send(new Discord.MessageEmbed().setDescription('Sa-as yazısını açmak için; `'+ prefix +'sa-as aç veya kapat`'))
 
   if (args[0] == 'aç') {
     db.set(`ss_${message.guild.id}`, 'açık')
-      message.channel.send(`Başarıyla botun \`Aleyküm selam\` yazmasını açtınız., Artık bot \`sa\` yazıldığında cevap verecek.`)
+      message.channel.send(new Discord.MessageEmbed().setDescription(`Başarıyla botun \`Aleyküm selam\` yazmasını açtınız., Artık bot \`sa\` yazıldığında cevap verecek.`))
 
   }
   if (args[0] == 'kapat') {
     db.delete(`ss_${message.guild.id}`, 'kapat')
-      message.channel.send(` Başarıyla \`Aleyküm selam\` yazmasını kapattınız, Artık bot \`sa\` yazıldığında cevap vermeyecek.`)
+      message.channel.send(new Discord.MessageEmbed().setDescription(` Başarıyla \`Aleyküm selam\` yazmasını kapattınız, Artık bot \`sa\` yazıldığında cevap vermeyecek.`))
 
   }
 
