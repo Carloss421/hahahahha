0c913945b10e273f5a6d81f09cff90524f6653dc
Glitch (alvibotaltyapi) <none>
2021-05-16 17:00:01 +0000

    🍝👋 Checkpoint ./komutlar/canlıdestek.js:15334932/9078 ./komutlar/istatistik.js:15334932/3

--- komutlar/canlıdestek.js
+++ komutlar/canlıdestek.js
@@ -1,39 +1,16 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-var ayarlar = require('../ayarlar.json');
-
-exports.run = async (client, message, args) => {
-  const codare = args.slice(0).join(' ');
-  if (codare.length < 1) return message.channel.send('`Talep Açma Sebebinizi Belirtiniz.`')
-    message.channel.send('`Destek Talebi Başarıyla Gönderildi,Yetkililerin Size Ulaşmasını BekLeyiniz.`');
-
-
-    var Hook = new Discord.WebhookClient("ID", "WEBHOOK TOKEN")
-
-    let embed = new Discord.MessageEmbed()
-    .setColor("GREEN")
-    .setThumbnail(message.author.avatarURL())
-    .setTitle(`Destek Talebi!`)
-    .setDescription(`
-      **Talep Eden: **`+message.author.tag+`
-      **Talep Sebebi: **\``+codare+`\`
-      `)
-    .setFooter(`Destek`)
-    .setTimestamp()
-    Hook.send(embed)
-
+const Discord = require('discord.js')
+const ayarlar = require('../ayarlar.json')
+exports.run = async(client, message, args) => {
+      message.channel.send(new Discord.MessageEmbed().setDescription(`${ayarlar.hata} Bu komut bakımdadır!`).setColor("#ff0000"))
+}
+  exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["canlı-destek"],
+  permLevel: 0
 };
-
-exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ['canlıdestek'],
-    permLevel: 0,  
-  };
-  
-  exports.help = {
-    name: 'canlı-estek',
-    description: 'Talebinizi belirterek destek alabilirsiniz.',
-    usage: 'destek-al ',
-   
-  };
+exports.help = {
+  name: 'canlıdestek',
+  description: 'Canlı Destek Tablebi Oluşturur.',
+  usage: 'canlıdestek'
+};
