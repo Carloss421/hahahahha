3f6a65bf315ddb6fe4445cf762a7c7b5c7ee343e
Glitch (alvibotaltyapi) <none>
2021-05-17 17:50:43 +0000

    🍶🚅 Checkpoint ./bot.js:15334932/1

--- bot.js
+++ bot.js
@@ -825,7 +825,7 @@ var hoşglend = new Discord.MessageEmbed()
 .addField(`Üye Adı`, `${member}`, true)
 client.channels.get(hoşgeldinK).send(hoşglend) 
 }});
-
+ 
 client.on("guildMemberRemove", async(member, message) => {
 if (db.has(`hoşgeldinK_${message.guild.id}`) === true) {
   let hoşgeldinK = db.fetch(`hoşgeldinK_${message.guild.id}`)
