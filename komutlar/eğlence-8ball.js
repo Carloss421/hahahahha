f65d196a8edd1873090d63d7b6c4977fc7bbc4b0
Glitch (alvibotaltyapi) <none>
2021-05-26 15:28:06 +0000

    🚋🏈 Checkpoint ./komutlar/eğlence-8ball.js:15334932/1051

--- komutlar/eğlence-8ball.js
+++ komutlar/eğlence-8ball.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-
+const db = require('quick.db')
 const cevaplar = [
     "evet",
     "hayır",
@@ -9,14 +9,32 @@ const cevaplar = [
     "imkansız"
 ];
 
-exports.run = function(client, message, args) {
-    var soru = args.join(' ');
+exports.run = async(client, message, args) => {
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgyODI2NzQ3NDE5MjU2NDI0NSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjIyMDQyNTMyfQ.912A76CIQeNWr9UIDD6ZLSkDZK_ZenMicw6KuombmhE  
+  
+ const snekfetch = require("snekfetch");
+snekfetch.get(`https://discordbots.org/api/bots/${client.user.id}/check?userId=${message.author.id}`)
+.set("Authorization", client.ayarlar.dbltoken)
+.then(response => {
+var check = response.body.voted;
+if(check == 1) {}
+  
+  let p = db.fetch(`prefix_${message.guild.id}`) || client.ayarlar.prefix;
+  var soru = args.join(' ');
 
     var cevap = cevaplar[Math.floor(Math.random() * cevaplar.length)];
 
-    if(!soru) return message.reply('Bir soru belirt. **Doğru Kullanım**: a!8ball <soru>')
+    if(!soru) return message.reply('Bir soru belirt. **Doğru Kullanım**: '+ p +'8ball <soru>')
     else message.channel.send(cevap)
 
+  } else {
+  let embed = new Discord.RichEmbed()
+        .setTitle('HATA')
+        .setColor('RANDOM')
+        .setDescription(`${client.emojis.get(client.emojiler.hayır)} **Hata**, bu komutu kullanmak için **12 saat aralıkla** **[BURADAN](https://discordbots.org/bot/${client.user.id}/vote)**  botu oylamanız gerekmektedir. Onaylanması **1-4** dakikayı bulabilir, lütfen bekleyin. `)
+      message.channel.send(embed)
+        return }});
+  
 };  
 
 exports.conf = {
