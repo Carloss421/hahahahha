1cbbed2cb9e2c1cda5bba5f354a8f82477627d5b
Glitch (alvibotaltyapi) <none>
2021-05-29 00:02:37 +0000

    🚈🐄 Checkpoint ./komutlar/fake-cezalı-rolKAPAT.js:15334932/189 ./komutlar/fake-hesap-sistemi.js:15334932/744 ./komutlar/fake-memberol.js:15334932/188

--- komutlar/fake-hesap-sistemi.js
+++ komutlar/fake-hesap-sistemi.js
@@ -9,7 +9,7 @@ message.channel.send(new Discord.MessageEmbed().setTitle('Alvi Fake Hesap Sistem
 \`${prefix}yeni-üye-rol [@rolEtiket]\` **Sunucunuza giren kullanıcılara verilen rolü etiketle**
 \`${prefix}fake-cezalı-rol [@rolEtiket]\` **Sunucunuz içerisinde ki cezalı rolünü etiketle**
 \`${prefix}yeni-üye-role-kapat\` **Ayarlı olan kayıtsız rol kapatır**
-\`${prefix}fake-cezalı-rol-kapat\` **Ayarlı olan fake cezalı rol kapatır**`).setThumbnail(message.author.avatarURL() ? message.author.avatarURL({dynamic: true}) : 'https://cdn.glitch.com/8e70d198-9ddc-40aa-b0c6-ccb4573f14a4%2F6499d2f1c46b106eed1e25892568aa55.png'));
+\`${prefix}fake-cezalı-rol-kapat\` **Ayarlı olan fake cezalı rol kapatır**`));
 
 };
 exports.conf = {
