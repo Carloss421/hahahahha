833fc4783ecd0009f5a8491f932c2a8ee9ae0a72
Glitch (alvibotaltyapi) <none>
2021-05-31 18:13:55 +0000

    💶🍂 Checkpoint ./komutlar/ekonomi-para.js:15334932/1446

--- komutlar/ekonomi-para.js
+++ komutlar/ekonomi-para.js
@@ -1,26 +1,26 @@
-const Discord = require('discord.js')
-const db = require('quick.db')
+const Discord = require("discord.js");
+const db = require("quick.db");
 exports.run = async (client, message, args) => {
+  let user = message.mentions.users.first() || message.author;
 
-          let user = message.mentions.users.first() || message.author
+  var cüzdan = db.fetch(`para_${user.id}`);
+  var banka = db.fetch(`bankapara_${user.id}`);
 
-  var cüzdan = db.fetch(`para_${user.id}`)
-  var banka = db.fetch(`bankapara_${user.id}`)   
-
-  var toplam= cüzdan+banka
-message.channel.send(new Discord.MessageEmbed()
-                  .setColor("RED")
-                  .setAuthor(user.tag, user.avatarURL({dynamic: true}))     
-                  .addField("Cüzdan",`${cüzdan ? cüzdan + 'TL' : "0TL"}`,true)
-                  .addField("Banka",`${banka ? banka + 'TL' : "0TL"}`,true))
-  }
-
+  var toplam = cüzdan + banka;
+  message.channel.send(
+    new Discord.MessageEmbed()
+      .setColor("RED")
+      .setAuthor(user.tag, user.avatarURL({ dynamic: true }))
+      .addField("Cüzdan", `${cüzdan ? cüzdan + "TL" : "0TL"}`, true)
+      .addField("Banka", `${banka ? banka + "TL" : "0TL"}`, true)
+  );
+};
 
 exports.conf = {
   enabled: true,
-  aliases: ["para","cüzdan","cash"],
+  aliases: ["para", "cüzdan", "cash"]
 };
 
 exports.help = {
-  name: 'Para',
+  name: "Para"
 };
