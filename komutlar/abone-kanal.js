ab340a1e9d3a615b2441019afcee85831171885d
Glitch (alvibotaltyapi) <none>
2021-08-18 11:03:16 +0000

    🎙🎒 Checkpoint ./komutlar/abone-kanal.js:15334932/1434

--- komutlar/abone-kanal.js
+++ komutlar/abone-kanal.js
@@ -7,7 +7,32 @@ exports.run = async(client, message, args) => {
 if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(new Discord.MessageEmbed().setDescription(`${ayarlar.oldu} ${message.author}, bu komutu kullanabilmek için \`YONETICI\` yetkisine sahip olmalısın!`))
 
 let p = db.fetch(`prefix_${message.guild.id}`) || ayarlar.prefix;  
-let veri = db.fetch(`aboneK_${message.guild.id}`)
+let v = db.fetch(`aboneK_${message.guild.id}`)
+let k = db.has(`aboneK_${message.guild.id}` ? "acik" : "kapali")
+
+if(k.kapali !== k.acik) {
+  const emb 
+}
+/*
+   if(message.author.id !== ayarlar.ownerID)  {
+    const embed = new Discord.MessageEmbed()
+    .setDescription(`**:x: Bu Komut Yapımcıma Özeldir !**`)
+    .setColor('BLUE')
+    return message.channel.send(embed).then(msg=>msg.delete(5000));
+    }
+
+
+////----------------------\\\\ PREMİUM KONTROL ////----------------------\\\\   
+if(message.author.id !== sahip) {
+  if(args[0] === 'kontrol') {
+  let açıkmı = await data.fetch(`premium.${message.guild.id}`)  
+  message.channel.send(new Discord.MessageEmbed()  
+.setColor('RANDOM')
+.setAuthor(message.guild.name, message.guild.iconURL)
+.setDescription(`Bu sunucu için **Premium** sistemi **${açıkmı ? 'aktif' : 'kapalı'}**!`)
+.setTimestamp())   
+}}
+*/
 
 };
 
