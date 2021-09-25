ebb1d3f21ea609ff8f9a35513dd308be407ad950
Glitch (alvibotaltyapi) <none>
2021-09-25 18:20:37 +0000

    👛☕️ Checkpoint ./komutlar/yardım.js:15334932/694 ./bot.js:15334932/4285

--- bot.js
+++ bot.js
@@ -68,67 +68,28 @@ client.elevation = message => {
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
-fs.readdir("./komutlar/", (err, files) => {
-  if (err) console.error(err);
-  log(`${files.length} komut yüklenecek.`);
-  files.forEach(f => {
-    let props = require(`./komutlar/${f}`);
-    log(`Yüklenen komut: ${props.help.name}.`);
-    client.commands.set(props.help.name, props);
-    props.conf.aliases.forEach(alias => {
-      client.aliases.set(alias, props.help.name);
+client.on('ready', () => {
+    console.log(client.user.tag); 
+    fs.readdir('./komutlar/', (error, files) => {
+        if (error) return console.log(error); 
+        files.forEach(file => { 
+            if (!file.endsWith('.js')) return; 
+            let fileProp = require('./komutlar/' + file);
+            client.api.applications(client.user.id).commands.post({
+                data: {
+                    name: fileProp.help.name, 
+                    description: fileProp.help.description, 
+                    options: fileProp.help.options
+                }
+            });
+            client.ws.on('INTERACTION_CREATE', async interaction => { 
+                const command = interaction.data.name.toLowerCase(); 
+                const args = interaction.data.options; 
+                if (command == fileProp.help.name.toLowerCase()) fileProp.run(client, interaction, args); 
+            });
+        });
     });
-  });
 });
-client.reload = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      delete require.cache[require.resolve(`./komutlar/${command}`)];
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.delete(command);
-      client.aliases.forEach((cmd, alias) => {
-        if (cmd === command) client.aliases.delete(alias);
-      });
-      client.commands.set(command, cmd);
-      cmd.conf.aliases.forEach(alias => {
-        client.aliases.set(alias, cmd.help.name);
-      });
-      resolve();
-    } catch (e) {
-      reject(e);
-    }
-  });
-};
-client.load = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.set(command, cmd);
-      cmd.conf.aliases.forEach(alias => {
-        client.aliases.set(alias, cmd.help.name);
-      });
-      resolve();
-    } catch (e) {
-      reject(e);
-    }
-  });
-};
-client.unload = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      delete require.cache[require.resolve(`./komutlar/${command}`)];
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.delete(command);
-      client.aliases.forEach((cmd, alias) => {
-        if (cmd === command) client.aliases.delete(alias);
-      });
-      resolve();
-    } catch (e) {
-      reject(e);
-    }
-  });
-};
-
 
 //     [-----------------> BOT ETIKET <---------------] \\
 client.on('message', async message => {
