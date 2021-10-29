eb6f35aeb393cadf558cbd303b4151b754660b0e
Glitch (alvibotaltyapi) <none>
2021-10-29 13:07:40 +0000

    🍱🍫 Checkpoint ./Languages/dil/en.js:15334932/6 ./Languages/dil/tr.json:15334932/34 ./Languages/dil/en.json:15334932/34 ./komutlar/Aowner-eval.js:15334932/223

--- komutlar/Aowner-eval.js
+++ komutlar/Aowner-eval.js
@@ -7,41 +7,44 @@ const dils = new dil("dil", "diller")
 
 exports.run = async (client, message, args) => {
 
-let en = require("../Languages/dil/en.js")
-let tr = require("../Languages/dil/tr.js")
-  
+let en = require("../Languages/dil/en")
+let tr = require("../Languages/dil/tr")
+
+let lang;
+
 var lg = dils.get(`dilang.${message.guild.id}`) || tr;
-if(lg == "en"){var lang = en}
+if(lg == "en"){lang = en}
+if(!lg){lang = tr}
 
 if(message.author.id !== ayarlar.ownerID) {
     const embed = new Discord.MessageEmbed()
-    .setDescription(lang.AownerEvalOWNERED)
+    .setDescription(lang.evalCommand.AownerEvalOWNERED)
     .setColor('BLUE')
     return message.channel.send(embed).then(msg=>msg.delete(3000));
     }
-let tokenuyari = lang.AownerEvalTOKENWARN;
+let tokenuyari = lang.evalCommand.AownerEvalTOKENWARN;
 var embed = new Discord.MessageEmbed().setColor('RANDOM');
 var code = args.join(' ');
 //var code2 = args.slice(1).join(' ') ||  args.join(' ');
 
  if(!args[0]) {
-   message.channel.send(lang.AownerEvalTESTCOMMANDSWARN)
+   message.channel.send(lang.evalCommand.AownerEvalTESTCOMMANDSWARN)
    return;
  };
 
  if(code.match(/(client.token)/g)) {
   let token_uyari = new Discord.MessageEmbed()
   .setColor('RANDOM')
-    .setDescription(`${lang.lg.AownerEvalERROR}\n\`\`\`xl\n${tokenuyari}\`\`\``)
+    .setDescription(`${lang.evalCommand.AownerEvalERROR}\n\`\`\`xl\n${tokenuyari}\`\`\``)
     message.channel.send(token_uyari);
      return;
  };
-if (args[0] === "ayarlar.token") code=lang.AownerEvalTOKENERRORED
-if (args[0] === "config.token") code=lang.AownerEvalTOKENERRORED
+if (args[0] === "ayarlar.token") code=lang.evalCommand.AownerEvalTOKENERRORED
+if (args[0] === "config.token") code=lang.evalCommand.AownerEvalTOKENERRORED
  if(code.match(/(client["token"])/g)) {
   let token_uyari2 = new Discord.MessageEmbed()
   .setColor('RANDOM')
-    .setDescription(`${lang.AownerEvalERROR}\n\`\`\`xl\n${tokenuyari}\`\`\``)
+    .setDescription(`${lang.evalCommand.AownerEvalERROR}\n\`\`\`xl\n${tokenuyari}\`\`\``)
     message.channel.send(token_uyari2);
      return;
  };
@@ -62,12 +65,12 @@ if (args[0] === "config.token") code=lang.AownerEvalTOKENERRORED
         else embed.setDescription(`\`\`\`xl\n${evaled}\n\`\`\``)
          let giren_cıkan = new Discord.MessageEmbed()
      .setColor('RANDOM')
-         .setDescription(`${lang.AownerEvalCODE}\n\`\`\`javascript\n${code}\n\`\`\``)
-         .setDescription(`${lang.AownerEvalCONCLUSION}\n\`\`\`xl\n${evaled}\`\`\``)
+         .setDescription(`${lang.evalCommand.AownerEvalCODE}\n\`\`\`javascript\n${code}\n\`\`\``)
+         .setDescription(`${lang.evalCommand.AownerEvalCONCLUSION}\n\`\`\`xl\n${evaled}\`\`\``)
          message.channel.send(giren_cıkan);
     } catch (err) {
     embed.setColor('RANDOM')
-        embed.setDescription(`${lang.AownerEvalERROReD}\n\`\`\`xl\n${err}\n\`\`\``)
+        embed.setDescription(`${lang.evalCommand.AownerEvalERROReD}\n\`\`\`xl\n${err}\n\`\`\``)
         message.channel.send(embed);
     };
 };
