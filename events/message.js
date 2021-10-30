e7a7fe701022ac9ca8de77f3231c822567f0d4cb
Glitch (alvibotaltyapi) <none>
2021-10-30 19:49:34 +0000

    🍏📖 Checkpoint ./events/message.js:15334932/10013

--- events/message.js
+++ events/message.js
@@ -92,28 +92,6 @@ var lang = en;
 var lang = tr;
   }
   
-if(!lg){
-if(cmd.name == "dil-ayarla") return;
-if(cmd.aliases == "language-set") return;
-if(cmd.aliases == "set-language") return;
-/*if(message.content.startsWith("a!dil-ayarla")) return;
-if(message.content.startsWith("a!language-set")) return;
-if(message.content.startsWith("a!set-language")) return;*/
-const embedd = new Discord.MessageEmbed()
-.setThumbnail(client.user.avatarURL())
-.setAuthor(client.user.username)
-
-.addField("<:hayir0:838855037161570375> **Hata | Error**",`
-**TR:** Botu kullanmadan önce dil seçmeniz gerekmektedir!
-Kullanım: **a!dil-ayarla Tr/En**
-
-**EN:** You must select the language before using the bot!
-Usage: **a!set-language En/Tr**`)
-.setFooter(message.author.tag, message.author.avatarURL())
-return message.channel.send({embed: embedd})
-}
-  
-  
     if (db.has(`karalist_${message.author.id}`) === true) {
     let embed = new Discord.MessageEmbed()
     .setColor("RANDOM")
@@ -125,6 +103,123 @@ return message.channel.send({embed: embedd})
 
   
   if (cmd) {
+if(!lg){
+if(message.content.startsWith("!dil-ayarla")) (client, message, args) => {
+let dilSeç = args[1]
+
+if(args[0] !== 'Tr' && args[0] !== 'tr' && args[0] !== 'TR' && args[0] !== 'tR' 
+&& args[0] !== 'En' && args[0] !== 'en' && args[0] !== 'EN' && args[0] !== 'eN') return message.channel.send(new Discord.MessageEmbed().setDescription(`<:hayir0:838855037161570375> Dil ayarlıcaksanız **!dil-ayarla Tr/En**/If you want to set language **!language-set En/Tr**`))
+
+if(args[0] === 'Tr' && 'tr' && 'TR' && 'tR'){
+if(!dilSeç) return message.reply("<:hayir0:838855037161570375> Lütfen bir dil belirtin!\nKullanım: **!dil-ayarla Tr/En**")
+if(lg) return message.reply("<:hayir0:838855037161570375> Dil zaten Türkçe!")
+const basarili = new Discord.MessageEmbed()
+.setThumbnail(client.user.avatarURL())
+.setAuthor(client.user.username, client.user.avatarURL())
+.setTitle("<:evet1:838854924875726898> BASARILI!")
+.setDescription(`Dil başarıyla **Türkçe** olarak ayarlandı!`)
+.setFooter("Dil değiştirmek için !dil-ayarla En/Tr",message.author.avatarURL()).setTimestamp()
+message.channel.send({embed:basarili})
+if(lg == "tr"){dils.delete(`dilang.${message.guild.id}`, "en")}
+dils.set(`dilang.${message.guild.id}`, "tr")};
+
+if(args[0] === 'En' && 'en' && 'EN' && 'eN'){
+if(!dilSeç) return message.reply("<:hayir0:838855037161570375> Please language enter!\Usage: **!language-set En/Tr**")
+if(lg) return message.reply("<:hayir0:838855037161570375> The language is already English!")
+const succesfly = new Discord.MessageEmbed()
+.setThumbnail(client.user.avatarURL())
+.setAuthor(client.user.username, client.user.avatarURL())
+.setTitle("<:evet1:838854924875726898> SUCCESLFY!")
+.setDescription(`Language successfully set to **English**!`)
+.setFooter("!language-set Tr/En to change language",message.author.avatarURL()).setTimestamp()
+message.channel.send({embed:succesfly})
+if(lg == "tr"){dils.delete(`dilang.${message.guild.id}`, "tr")}
+dils.set(`dilang.${message.guild.id}`, "en")};
+};
+
+if(message.content.startsWith("!set-language")) (client, message, args) => {
+let dilSeç = args[1]
+
+if(args[0] !== 'Tr' && args[0] !== 'tr' && args[0] !== 'TR' && args[0] !== 'tR' 
+&& args[0] !== 'En' && args[0] !== 'en' && args[0] !== 'EN' && args[0] !== 'eN') return message.channel.send(new Discord.MessageEmbed().setDescription(`<:hayir0:838855037161570375> Dil ayarlıcaksanız **!dil-ayarla Tr/En**/If you want to set language **!language-set En/Tr**`))
+
+if(args[0] === 'Tr' && 'tr' && 'TR' && 'tR'){
+if(!dilSeç) return message.reply("<:hayir0:838855037161570375> Lütfen bir dil belirtin!\nKullanım: **!dil-ayarla Tr/En**")
+if(lg) return message.reply("<:hayir0:838855037161570375> Dil zaten Türkçe!")
+const basarili = new Discord.MessageEmbed()
+.setThumbnail(client.user.avatarURL())
+.setAuthor(client.user.username, client.user.avatarURL())
+.setTitle("<:evet1:838854924875726898> BASARILI!")
+.setDescription(`Dil başarıyla **Türkçe** olarak ayarlandı!`)
+.setFooter("Dil değiştirmek için !dil-ayarla En/Tr",message.author.avatarURL()).setTimestamp()
+message.channel.send({embed:basarili})
+if(lg == "tr"){dils.delete(`dilang.${message.guild.id}`, "en")}
+dils.set(`dilang.${message.guild.id}`, "tr")};
+
+if(args[0] === 'En' && 'en' && 'EN' && 'eN'){
+if(!dilSeç) return message.reply("<:hayir0:838855037161570375> Please language enter!\Usage: **a!language-set En/Tr**")
+if(lg) return message.reply("<:hayir0:838855037161570375> The language is already English!")
+const succesfly = new Discord.MessageEmbed()
+.setThumbnail(client.user.avatarURL())
+.setAuthor(client.user.username, client.user.avatarURL())
+.setTitle("<:evet1:838854924875726898> SUCCESLFY!")
+.setDescription(`Language successfully set to **English**!`)
+.setFooter("!language-set Tr/En to change language",message.author.avatarURL()).setTimestamp()
+message.channel.send({embed:succesfly})
+if(lg == "tr"){dils.delete(`dilang.${message.guild.id}`, "tr")}
+dils.set(`dilang.${message.guild.id}`, "en")};
+};
+
+if(message.content.startsWith("!language-set")) (client, message, args) => {
+
+
+let dilSeç = args[1]
+
+if(args[0] !== 'Tr' && args[0] !== 'tr' && args[0] !== 'TR' && args[0] !== 'tR' 
+&& args[0] !== 'En' && args[0] !== 'en' && args[0] !== 'EN' && args[0] !== 'eN') return message.channel.send(new Discord.MessageEmbed().setDescription(`<:hayir0:838855037161570375> Dil ayarlıcaksanız **a!dil-ayarla Tr/En**/If you want to set language **a!language-set En/Tr**`))
+
+if(args[0] === 'Tr' && 'tr' && 'TR' && 'tR'){
+if(!dilSeç) return message.reply("<:hayir0:838855037161570375> Lütfen bir dil belirtin!\nKullanım: **a!dil-ayarla Tr/En**")
+if(lg) return message.reply("<:hayir0:838855037161570375> Dil zaten Türkçe!")
+const basarili = new Discord.MessageEmbed()
+.setThumbnail(client.user.avatarURL())
+.setAuthor(client.user.username, client.user.avatarURL())
+.setTitle("<:evet1:838854924875726898> BASARILI!")
+.setDescription(`Dil başarıyla **Türkçe** olarak ayarlandı!`)
+.setFooter("Dil değiştirmek için a!dil-ayarla En/Tr",message.author.avatarURL()).setTimestamp()
+message.channel.send({embed:basarili})
+if(lg == "tr"){dils.delete(`dilang.${message.guild.id}`, "en")}
+dils.set(`dilang.${message.guild.id}`, "tr")};
+
+if(args[0] === 'En' && 'en' && 'EN' && 'eN'){
+if(!dilSeç) return message.reply("<:hayir0:838855037161570375> Please language enter!\Usage: **a!language-set En/Tr**")
+if(lg) return message.reply("<:hayir0:838855037161570375> The language is already English!")
+const succesfly = new Discord.MessageEmbed()
+.setThumbnail(client.user.avatarURL())
+.setAuthor(client.user.username, client.user.avatarURL())
+.setTitle("<:evet1:838854924875726898> SUCCESLFY!")
+.setDescription(`Language successfully set to **English**!`)
+.setFooter("a!language-set Tr/En to change language",message.author.avatarURL()).setTimestamp()
+message.channel.send({embed:succesfly})
+if(lg == "tr"){dils.delete(`dilang.${message.guild.id}`, "tr")}
+dils.set(`dilang.${message.guild.id}`, "en")};
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const embedd = new Discord.MessageEmbed()
+.setThumbnail(client.user.avatarURL())
+.setAuthor(client.user.username)
+
+.addField("<:hayir0:838855037161570375> **Hata | Error**",`
+**TR:** Botu kullanmadan önce dil seçmeniz gerekmektedir!
+Kullanım: **!dil-ayarla Tr/En**
+
+**EN:** You must select the language before using the bot!
+Usage: **!set-language En/Tr**`)
+.setFooter(message.author.tag, message.author.avatarURL())
+return message.channel.send({embed: embedd})
+} 
+    
     
   let bakım = await db.fetch('bakım');
   if(message.author.id !== ayarlar.ownerID)
