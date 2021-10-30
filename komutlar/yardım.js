c00d71ece69e9e30db28afe6703513432a163113
Glitch (alvibotaltyapi) <none>
2021-10-30 20:12:46 +0000

    ⛑🚀 Checkpoint ./events/message.js:15334932/10529 ./komutlar/Dil-ayarla.js:15334932/2291 ./komutlar/Aowner-eval.js:15334932/484 ./komutlar/Aowner-karaliste.js:15334932/484 ./komutlar/Aowner-komutREBOOT.js:15334932/484 ./komutlar/Aowner-reboot.js:15334932/487 ./komutlar/abone-rol.js:15334932/484 ./komutlar/abone-sistemi.js:15334932/487 ./komutlar/abone-yetkili.js:15334932/481 ./komutlar/yardım.js:15334932/1615

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,26 +1,54 @@
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
+const dil = require("../Languages/dil");
+const dils = new dil("dil", "diller");
 
-exports.run = function(client, message, args) {
+exports.run = async(client, message, args) => {
 const db = require('quick.db')
-let prefix = db.fetch(`prefix_${message.guild.id}`) || ayarlar.prefix;
+
+   let en = require("../Languages/dil/en.json");
+  let tr = require("../Languages/dil/tr.json");
+
+  var lg = dils.get(`dilang.${message.guild.id}`)
+  if (lg == "en") {
+var lang = en;
+  }
+  if (lg == "tr") {
+var lang = tr;
+  }
+
+if(!lg){
+const embedd = new Discord.MessageEmbed()
+.setThumbnail(client.user.avatarURL())
+.setAuthor(client.user.username)
+
+.addField("<:hayir0:838855037161570375> **Hata | Error**",`
+**TR:** Botu kullanmadan önce dil seçmeniz gerekmektedir!
+Kullanım: **a!dil-ayarla Tr/En**
+
+**EN:** You must select the language before using the bot!
+Usage: **a!set-language En/Tr**`)
+.setFooter(message.author.tag, message.author.avatarURL())
+return message.channel.send({embed: embedd})
+};
+
   
 let embed = new Discord.MessageEmbed()
-.setTitle('Alvi - Yardım')
+.setTitle(`Alvi - ${lang.helpMenu.hTitle}`)
 .setColor('RANDOM')
 .setDescription(`
-Alvi'de bulduğunuz hata,çalışmama veya açıkları bildirmek için **${prefix}hata-bildir**
+Alvi'de bulduğunuz hata,çalışmama veya açıkları bildirmek için **a!hata-bildir**
 komutunu kullanarak hatayı bildirebilirsiniz eğer komutu kullanmayı bilmiyorsanız [Destek Sunucumuza](https://discord.gg/NAzGC2cxXR) gelerekte bildirebilirsiniz.
 
-**kullanıcı menüsü** \`${prefix}yardım-kullanıcı\` Kullanıcı menüsünü açar.
-**eğlence menüsü** \`${prefix}yardım-eğlence\` Canı sıkılanlar için yararlı bir menü.
-**moderasyon menüsü** \`${prefix}yardım-moderasyon\` Yetkilere kolaylık olsun diye ayarlandı.
-**yapımcı menüsü** \`${prefix}yardım-yapımcı | ${prefix}yardım-owner\` Yapımcı'nın kullanabiliceği komutları açar.
-**güncelleme menüsü** \`${prefix}güncellemeler | ${prefix}uptades\` Bot'a gelen güncellemeleri gösterir.
-**güncelleme versionları** \`${prefix}güncelleme-v | ${prefix}güncelleme-version\` Gelen güncelleme versionlarını gösterir.
-**bot menüsü** \`${prefix}yardım-bot\` Bot'un davet linkini gibi şeyleri gösterir.
+**kullanıcı menüsü** \`a!yardım-kullanıcı\` Kullanıcı menüsünü açar.
+**eğlence menüsü** \`a!yardım-eğlence\` Canı sıkılanlar için yararlı bir menü.
+**moderasyon menüsü** \`a!yardım-moderasyon\` Yetkilere kolaylık olsun diye ayarlandı.
+**yapımcı menüsü** \`a!yardım-yapımcı | a!yardım-owner\` Yapımcı'nın kullanabiliceği komutları açar.
+**güncelleme menüsü** \`a!güncellemeler | a!uptades\` Bot'a gelen güncellemeleri gösterir.
+**güncelleme versionları** \`a!güncelleme-v | a!güncelleme-version\` Gelen güncelleme versionlarını gösterir.
+**bot menüsü** \`a!yardım-bot\` Bot'un davet linkini gibi şeyleri gösterir.
 
-~~**ekonomi menüsü** \`${prefix}yardım-ekonomi\` Eğlence amaçlı ayarlanmıştır.~~
+~~**ekonomi menüsü** \`a!yardım-ekonomi\` Eğlence amaçlı ayarlanmıştır.~~
 `)
 message.channel.send(embed)
 };
