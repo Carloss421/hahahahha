2b72ed7418016de8ea9e9802ee53886de009c4d7
Glitch (alvibotaltyapi) <none>
2021-10-30 20:00:54 +0000

    😹🚎 Checkpoint ./events/message.js:15334932/8698

--- events/message.js
+++ events/message.js
@@ -57,7 +57,7 @@ const dil = require("../Languages/dil");
 const dils = new dil("dil", "diller");
 
 let talkedRecently = new Set();
-module.exports = async message => {
+module.exports = async (message, args) => {
 if(message.author.bot) return
 
   if (talkedRecently.has(message.author.id)) {
@@ -104,7 +104,8 @@ var lang = tr;
   
   if (cmd) {
 if(!lg){
-if(message.content.startsWith("!dil-ayarla")) (client, message, args) => {
+if(message.content.startsWith("dil-ayarla")){
+
 let dilSeç = args[1]
 
 if(args[0] !== 'Tr' && args[0] !== 'tr' && args[0] !== 'TR' && args[0] !== 'tR' 
@@ -123,6 +124,7 @@ message.channel.send({embed:basarili})
 if(lg == "tr"){dils.delete(`dilang.${message.guild.id}`, "en")}
 dils.set(`dilang.${message.guild.id}`, "tr")};
 
+  
 if(args[0] === 'En' && 'en' && 'EN' && 'eN'){
 if(!dilSeç) return message.reply("<:hayir0:838855037161570375> Please language enter!\Usage: **!language-set En/Tr**")
 if(lg) return message.reply("<:hayir0:838855037161570375> The language is already English!")
@@ -134,10 +136,29 @@ const succesfly = new Discord.MessageEmbed()
 .setFooter("!language-set Tr/En to change language",message.author.avatarURL()).setTimestamp()
 message.channel.send({embed:succesfly})
 if(lg == "tr"){dils.delete(`dilang.${message.guild.id}`, "tr")}
-dils.set(`dilang.${message.guild.id}`, "en")};
-};
+dils.set(`dilang.${message.guild.id}`, "en")}};
 
-if(message.content.startsWith("!set-language")) (client, message, args) => {
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+if(message.content.startsWith("set-language")){
+  
 let dilSeç = args[1]
 
 if(args[0] !== 'Tr' && args[0] !== 'tr' && args[0] !== 'TR' && args[0] !== 'tR' 
@@ -156,8 +177,9 @@ message.channel.send({embed:basarili})
 if(lg == "tr"){dils.delete(`dilang.${message.guild.id}`, "en")}
 dils.set(`dilang.${message.guild.id}`, "tr")};
 
+  
 if(args[0] === 'En' && 'en' && 'EN' && 'eN'){
-if(!dilSeç) return message.reply("<:hayir0:838855037161570375> Please language enter!\Usage: **a!language-set En/Tr**")
+if(!dilSeç) return message.reply("<:hayir0:838855037161570375> Please language enter!\Usage: **!language-set En/Tr**")
 if(lg) return message.reply("<:hayir0:838855037161570375> The language is already English!")
 const succesfly = new Discord.MessageEmbed()
 .setThumbnail(client.user.avatarURL())
@@ -167,44 +189,80 @@ const succesfly = new Discord.MessageEmbed()
 .setFooter("!language-set Tr/En to change language",message.author.avatarURL()).setTimestamp()
 message.channel.send({embed:succesfly})
 if(lg == "tr"){dils.delete(`dilang.${message.guild.id}`, "tr")}
-dils.set(`dilang.${message.guild.id}`, "en")};
-};
+dils.set(`dilang.${message.guild.id}`, "en")}};
 
-if(message.content.startsWith("!language-set")) (client, message, args) => {
 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+if(message.content.startsWith("language-set")){
 
 let dilSeç = args[1]
 
 if(args[0] !== 'Tr' && args[0] !== 'tr' && args[0] !== 'TR' && args[0] !== 'tR' 
-&& args[0] !== 'En' && args[0] !== 'en' && args[0] !== 'EN' && args[0] !== 'eN') return message.channel.send(new Discord.MessageEmbed().setDescription(`<:hayir0:838855037161570375> Dil ayarlıcaksanız **a!dil-ayarla Tr/En**/If you want to set language **a!language-set En/Tr**`))
+&& args[0] !== 'En' && args[0] !== 'en' && args[0] !== 'EN' && args[0] !== 'eN') return message.channel.send(new Discord.MessageEmbed().setDescription(`<:hayir0:838855037161570375> Dil ayarlıcaksanız **!dil-ayarla Tr/En**/If you want to set language **!language-set En/Tr**`))
 
 if(args[0] === 'Tr' && 'tr' && 'TR' && 'tR'){
-if(!dilSeç) return message.reply("<:hayir0:838855037161570375> Lütfen bir dil belirtin!\nKullanım: **a!dil-ayarla Tr/En**")
+if(!dilSeç) return message.reply("<:hayir0:838855037161570375> Lütfen bir dil belirtin!\nKullanım: **!dil-ayarla Tr/En**")
 if(lg) return message.reply("<:hayir0:838855037161570375> Dil zaten Türkçe!")
 const basarili = new Discord.MessageEmbed()
 .setThumbnail(client.user.avatarURL())
 .setAuthor(client.user.username, client.user.avatarURL())
 .setTitle("<:evet1:838854924875726898> BASARILI!")
 .setDescription(`Dil başarıyla **Türkçe** olarak ayarlandı!`)
-.setFooter("Dil değiştirmek için a!dil-ayarla En/Tr",message.author.avatarURL()).setTimestamp()
+.setFooter("Dil değiştirmek için !dil-ayarla En/Tr",message.author.avatarURL()).setTimestamp()
 message.channel.send({embed:basarili})
 if(lg == "tr"){dils.delete(`dilang.${message.guild.id}`, "en")}
 dils.set(`dilang.${message.guild.id}`, "tr")};
 
+  
 if(args[0] === 'En' && 'en' && 'EN' && 'eN'){
-if(!dilSeç) return message.reply("<:hayir0:838855037161570375> Please language enter!\Usage: **a!language-set En/Tr**")
+if(!dilSeç) return message.reply("<:hayir0:838855037161570375> Please language enter!\Usage: **!language-set En/Tr**")
 if(lg) return message.reply("<:hayir0:838855037161570375> The language is already English!")
 const succesfly = new Discord.MessageEmbed()
 .setThumbnail(client.user.avatarURL())
 .setAuthor(client.user.username, client.user.avatarURL())
 .setTitle("<:evet1:838854924875726898> SUCCESLFY!")
 .setDescription(`Language successfully set to **English**!`)
-.setFooter("a!language-set Tr/En to change language",message.author.avatarURL()).setTimestamp()
+.setFooter("language-set Tr/En to change language",message.author.avatarURL()).setTimestamp()
 message.channel.send({embed:succesfly})
 if(lg == "tr"){dils.delete(`dilang.${message.guild.id}`, "tr")}
-dils.set(`dilang.${message.guild.id}`, "en")};
-};
+dils.set(`dilang.${message.guild.id}`, "en")}};
 
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const embedd = new Discord.MessageEmbed()
 .setThumbnail(client.user.avatarURL())
@@ -218,7 +276,7 @@ Kullanım: **!dil-ayarla Tr/En**
 Usage: **!set-language En/Tr**`)
 .setFooter(message.author.tag, message.author.avatarURL())
 return message.channel.send({embed: embedd})
-} 
+};
     
     
   let bakım = await db.fetch('bakım');
