ba41eb071b6c5c374c2d8e369694550c62c172f8
Glitch (alvibotaltyapi) <none>
2022-01-09 20:00:20 +0000

    🍉🎇 Checkpoint ./komutlar/canlıdestek.js:15334932/484 ./Languages/dil/tr.json:15334932/67 ./Languages/dil/en.json:15334932/124

--- komutlar/canlıdestek.js
+++ komutlar/canlıdestek.js
@@ -1,7 +1,23 @@
 const Discord = require('discord.js')
 const ayarlar = require('../ayarlar.json')
+const dil = require("../Languages/dil");
+const dils = new dil("dil", "diller");
+
 exports.run = async(client, message, args) => {
-      message.channel.send(new Discord.MessageEmbed().setDescription(`${ayarlar.hata} Bu komut bakımdadır!`).setColor("#ff0000"))
+
+    
+  let en = require("../Languages/dil/en.json");
+  let tr = require("../Languages/dil/tr.json");
+
+  var lg = dils.get(`dilang.${message.guild.id}`)
+  if (lg == "en") {
+var lang = en;
+  }
+  if (lg == "tr") {
+var lang = tr;
+  }
+
+message.channel.send(new Discord.MessageEmbed().setDescription(`${ayarlar.hata} ${lang.lsupport.CD}`).setColor("#ff0000"))
 }
   exports.conf = {
   enabled: true,
