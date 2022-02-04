154bf18517cb1fb0d9a7272ba3b8e9fffeaca225
Glitch (alvibotaltyapii) <none>
2022-02-04 20:02:56 +0000

    🙆🏓 Checkpoint ./komutlar/abone-yetkili.js:15334932/1195 ./komutlar/abone-sistemi.js:15334932/936

--- komutlar/abone-sistemi.js
+++ komutlar/abone-sistemi.js
@@ -1,41 +1,13 @@
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 const db = require('quick.db')
-const dil = require("../Languages/dil");
-const dils = new dil("dil", "diller");
 
 exports.run = function(client, message, args) {
 
-  let en = require("../Languages/dil/en.json");
-  let tr = require("../Languages/dil/tr.json");
-
-  var lg = dils.get(`dilang.${message.guild.id}`)
-  if (lg == "en") {
-var lang = en;
-  }
-  if (lg == "tr") {
-var lang = tr;
-  }
-  
-  if(!lg){
-const embedd = new Discord.MessageEmbed()
-.setThumbnail(client.user.avatarURL())
-.setAuthor(client.user.username)
-
-.addField("<:hayir0:838855037161570375> **Hata | Error**",`
-**TR:** Botu kullanmadan önce dil seçmeniz gerekmektedir!
-Kullanım: **a!dil-ayarla Tr/En**
-
-**EN:** You must select the language before using the bot!
-Usage: **a!set-language En/Tr**`)
-.setFooter(message.author.tag, message.author.avatarURL())
-return message.channel.send({embed: embedd})
-};
-
 let embed = new Discord.MessageEmbed()
-.setTitle(`Alvi - ${lang.subscribe.sSYSTEm}`)
+.setTitle(`Alvi - ""`)
 .setColor("RANDOM")
-.setDescription(lang.subscribe.sSYSTEM)
+.setDescription("")
 message.channel.send(embed)
 };
 
